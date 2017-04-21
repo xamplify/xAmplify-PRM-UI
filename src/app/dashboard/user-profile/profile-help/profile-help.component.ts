52d2ee8b6643977f41f3384f48eff71ac15b0f98
nareshcharykotha <knaresh@xtremand.com>
2017-04-21 12:18:40 +0530

    added the profile-help component in user-profile.

--- src/app/dashboard/user-profile/profile-help/profile-help.component.ts
+++ src/app/dashboard/user-profile/profile-help/profile-help.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var Metronic : any;
+declare var Layout : any;
+declare var Demo : any;
+declare var Profile : any;
+
 @Component({
   selector: 'app-profile-help',
   templateUrl: './profile-help.component.html',
-  styleUrls: ['./profile-help.component.css']
+  styleUrls: ['../../../../assets/css/bootstrap-fileinput.css','../../../../assets/css/profile.css']
 })
 export class ProfileHelpComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
+      try{
+          Metronic.init();
+          Layout.init();
+          Demo.init(); 
+          Profile.init(); 
+     }
+     catch(err){}
+ }       
 
 }
