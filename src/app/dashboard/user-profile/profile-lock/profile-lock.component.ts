2b8cfba1f6622cfd7a845d5c9a23aee95cbb3d53
nareshcharykotha <knaresh@xtremand.com>
2017-04-21 15:53:23 +0530

    completed the user-profile module.

--- src/app/dashboard/user-profile/profile-lock/profile-lock.component.ts
+++ src/app/dashboard/user-profile/profile-lock/profile-lock.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+declare var Metronic : any;
+declare var Layout : any;
+declare var Demo : any;
+declare var Profile : any;
 
 @Component({
   selector: 'app-profile-lock',
@@ -10,6 +14,14 @@ export class ProfileLockComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      try {
+          Metronic.init();
+          Layout.init(); 
+          Demo.init();
+      }
+      catch (err) {
+          console.log("error");
+      }
   }
 
 }
