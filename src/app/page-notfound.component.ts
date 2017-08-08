aca308148514a7cfb90bceaf85edfbd57bf0401c
KothaSathish <sathish7chary@gmail.com>
2017-08-08 22:01:08 +0530

    .htaccess file and page not found page added

--- src/app/page-notfound.component.ts
+++ src/app/page-notfound.component.ts
@@ -4,14 +4,12 @@ import { Router } from '@angular/router';
 @Component({
   selector:  'not-found',
   template: `
-    <h3 style="background: white">{{title}} go back or click on <a (click)="homePage()">home</a></h3>
-    <img src="assets/images/404Page.jpg" style="width: 100%">
+    <h3 style="background: white">{{title}} Please go back or click on <a (click)="homePage()">DashBoard</a></h3>
+    <img src="assets/images/404Page.jpg" style="width: 100%; height: 566px;">
     `
 })
 export class NotFoundPageComponent {
-  title = 'Not Found found this URL';
+  title = 'The page was Not Found for this URL !';
   constructor(public router: Router ) { }
-  homePage() {
-    this.router.navigate(['./home/dashboard']);
-  }
+  homePage() { this.router.navigate(['./home/dashboard']); }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,20 +18,20 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/set';
+/** IE9, IE10 and IE11 requires all of the following polyfills.Un commented these lines for run the project in IE */
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
@@ -39,6 +39,10 @@
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
+/* added these 3 import staments for run the xtremand App in IE 10 below*/
+import 'core-js/client/shim'; // or load it before other angular2 & zone.js
+import 'zone.js';
+import 'reflect-metadata';
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
@@ -65,4 +69,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-// import 'intl';  // Run `npm install --save intl`.
+ // import 'intl';  // Run `npm install --save intl`.
