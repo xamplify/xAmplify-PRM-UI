150c8588a1e018d2357c7cd64c24cd9442f48640
KothaSathish <sathish7chary@gmail.com>
2017-08-17 21:24:57 +0530

    Resolved the Same Url Reload issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit  } from '@angular/core';
-import { Logger } from "angular2-logger/core";
-import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
+import { Component, OnInit } from '@angular/core';
+import { Logger } from 'angular2-logger/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    
-    constructor(private logger: Logger, private slimLoadingBarService: SlimLoadingBarService){
-        logger.level = logger.Level.LOG; 
-         this.logger.log("AppComponent constructor");
+
+    constructor(private logger: Logger, private slimLoadingBarService: SlimLoadingBarService, private router: Router, ) {
+        logger.level = logger.Level.LOG;
+        this.logger.log('AppComponent constructor');
     }
     ngOnInit() {
-        this.logger.log("AppComponent initialized");
+        this.logger.log('AppComponent initialized');
+        // reloading the same url with in the application
+        this.router.routeReuseStrategy.shouldReuseRoute = function () {
+            return false;
+        };
+        this.router.events.subscribe((evt) => {
+            if (evt instanceof NavigationEnd) {
+                this.router.navigated = false;
+                window.scrollTo(0, 0);
+            }
+        });
     }
 }
