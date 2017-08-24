7d105129db445816b4e26d2c3514e5e306c97fe7
KothaSathish <sathish7chary@gmail.com>
2017-08-24 10:47:39 +0530

    updated xtremand logger service

--- src/app/error-pages/xtremand-logger.service.ts
+++ src/app/error-pages/xtremand-logger.service.ts
@@ -8,6 +8,18 @@ export class XtremandLogger {
 
 	constructor(public router: Router, public logger: Logger) { }
 
+	log(...logMessages) {
+		for (const logMessage of logMessages) {
+			this.logger.warn(logMessage);
+		}
+	}
+
+	info(...infoMessages) {
+		for (const infoMessage of infoMessages) {
+			this.logger.info(infoMessage);
+		}
+	}
+
 	warn(...warnMessages) {
 		for (const warnMessage of warnMessages) {
 			this.logger.warn(warnMessage);
@@ -20,16 +32,12 @@ export class XtremandLogger {
 		}
 	}
 
-	info(...infoMessages) {
-		for (const infoMessage of infoMessages) {
-			this.logger.info(infoMessage);
-		}
-	}
 	error(...errorMessages) {
 		for (const err of errorMessages) {
 			this.logger.error(err);
 		}
 	}
+	
 	errorPage(error: any) {
 		this.router.navigate(['/home/error-occured-page/', error.status]);
 	}
