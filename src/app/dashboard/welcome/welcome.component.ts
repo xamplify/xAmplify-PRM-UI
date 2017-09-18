7f8199733d256374d0e0c4bcb8b5006b138dc61c
manas-xtremand <smanas@xtremand.com>
2017-09-18 23:23:57 +0530

    welcome page report functionality added.

--- src/app/dashboard/welcome/welcome.component.ts
+++ src/app/dashboard/welcome/welcome.component.ts
@@ -3,8 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../core/services/user.service';
 import { AuthenticationService } from '../../core/services/authentication.service';
 import { ReferenceService } from '../../core/services/reference.service';
+import { DashboardService } from '../dashboard.service';
 
 import { UserDefaultPage } from '../../core/models/user-default-page';
+import { DashboardReport } from '../../core/models/dashboard-report';
 
 @Component( {
     selector: 'app-welcome',
@@ -12,8 +14,16 @@ import { UserDefaultPage } from '../../core/models/user-default-page';
     styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-    userDefaultPage: UserDefaultPage = new UserDefaultPage();
-    constructor( private userService: UserService, private authenticationService: AuthenticationService, private referenceService: ReferenceService ) { }
+    
+    userDefaultPage: UserDefaultPage;
+    dashboardReport: DashboardReport;
+    loggedInUserId: number;
+
+    constructor( private userService: UserService, private authenticationService: AuthenticationService, 
+            private referenceService: ReferenceService, private dashboardService: DashboardService ) {
+        this.dashboardReport = new DashboardReport();
+        this.userDefaultPage = new UserDefaultPage();
+    }
     
     getDefaultPage( userId: number ) {
         this.userService.getUserDefaultPage( userId )
@@ -44,10 +54,28 @@ export class WelcomeComponent implements OnInit {
             () => { }
             );
     }
+    
+    dashboardReports(loggedInUserId: number){
+        this.dashboardService.loadDashboardReportsCount(this.loggedInUserId)
+            .subscribe(
+            data => {
+                this.dashboardReport.totalViews = data.totalVideoViewsCount;
+                this.dashboardReport.totalContacts = data.totalcontactsCount;
+                this.dashboardReport.totalUploadedvideos = data.totalVideosCount;
+                this.dashboardReport.toalEmailTemplates = data.totalEmailTemplatesCount;
+                this.dashboardReport.totalCreatedCampaigns = data.totalCampaignsCount;
+                this.dashboardReport.totalSocialAccounts = data.totalSocialConnectionsCount;
+            },
+            error => console.log(error),
+            () => console.log("dashboard reports counts completed")
+            );
+    
+    }
 
     ngOnInit() {
-        const userId = this.authenticationService.getUserId();
-        this.getDefaultPage(userId);
+        this.loggedInUserId = this.authenticationService.getUserId();
+        this.getDefaultPage(this.loggedInUserId);
+        this.dashboardReports(this.loggedInUserId);
     }
 
 }
