4cf1fee274514aed611ac4974398cf5b954f682f
manas-xtremand <smanas@xtremand.com>
2017-09-21 00:00:23 +0530

    loader UI enhancement and Error page code clean up.

--- src/app/error-pages/error-pages.component.ts
+++ src/app/error-pages/error-pages.component.ts
@@ -9,12 +9,6 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class ErrorPagesComponent implements OnInit, OnDestroy {
   errorStatus: number;
   private sub: any;
-  public Error400 = false;
-  public Error403 = false;
-  public Error500 = false;
-  public Error406 = false;
-  public Error409 = false;
-  public Error503 = false;
   public ErrorUndefined = false;
   public errorDifferent = false;
   public errorMessage: string;
@@ -27,18 +21,8 @@ export class ErrorPagesComponent implements OnInit, OnDestroy {
     this.sub = this.route.params.subscribe(params => {
       this.errorStatus = +params['errorStatusId']; // (+) converts string 'id' to a number
     });
-    if (this.errorStatus === 400) {
-      this.Error400 = true;
-    } else if (this.errorStatus === 406) {
-      this.Error406 = true;
-    } else if (this.errorStatus === 409) {
-      this.Error409 = true;
-    } else if (this.errorStatus === 500) {
-      this.Error500 = true;
-    } else if (this.errorStatus === 503) {
+    if (this.errorStatus === 503) {
       this.router.navigate(['/serviceunavailable']);
-    } else if (this.errorStatus === 403) {
-      this.Error403 = true;
     } else if(this.errorStatus === 0) {
        this.router.navigate(['/serviceunavailable']);
     }else if (this.isNumber(this.errorStatus)) {
