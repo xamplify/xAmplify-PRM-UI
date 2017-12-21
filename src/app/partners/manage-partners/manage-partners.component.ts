0053bb7982f8739aba0fda43ead0082efcb262d2
nareshcharykotha <knaresh@xtremand.com>
2017-12-21 11:41:51 +0530

    Partner Contact adding new partner UI added and downolad empty Csv functionality added.

--- src/app/partners/manage-partners/manage-partners.component.ts
+++ src/app/partners/manage-partners/manage-partners.component.ts
@@ -7,6 +7,8 @@ import { ContactService } from '../../contacts/services/contact.service';
 import { AuthenticationService } from '../../core/services/authentication.service';
 import { ReferenceService } from '../../core/services/reference.service';
 
+declare var $: any;
+
 @Component( {
     selector: 'app-manage-partners',
     templateUrl: './manage-partners.component.html',
@@ -15,12 +17,16 @@ import { ReferenceService } from '../../core/services/reference.service';
 } )
 export class ManagePartnersComponent implements OnInit {
     loggedInUserId: number;
+    validEmailPatternSuccess: boolean = true;
     user: User;
+    checkingForEmail:boolean;
+    addPartnerUser: User = new User();
     constructor( private authenticationService: AuthenticationService,
         private referenceService: ReferenceService,
         private contactService: ContactService,
         private pagination: Pagination ) {
         this.user = new User();
+        this.addPartnerUser.country = (this.referenceService.countries[0]);
     }
 
     listPartners( userId: number ) {
@@ -44,6 +50,45 @@ export class ManagePartnersComponent implements OnInit {
             () => console.log( 'loadContacts() finished' )
         );
     }
+    
+    checkingEmailPattern( emailId: string ) {
+        this.validEmailPatternSuccess = false;
+        if ( this.validateEmailAddress( emailId ) ) {
+            this.validEmailPatternSuccess = true;
+        } else {
+            this.validEmailPatternSuccess = false;
+        }
+    }
+
+    validateEmailAddress( emailId: string ) {
+        var EMAIL_ID_PATTERN = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return EMAIL_ID_PATTERN.test( emailId );
+    }
+    
+    validateEmail(emailId: string){
+        if(this.validateEmailAddress( emailId )){
+            this.checkingForEmail = true;
+            this.validEmailPatternSuccess = true;
+        }
+        else{
+            this.checkingForEmail = false;
+        }
+    }
+    
+    addPartnerModalOpen(){
+        $( "#addPartnerModal" ).show();
+        this.addPartnerUser.country = (this.referenceService.countries[0]);
+    }
+    
+    addPartnerModalClose(){
+        $('#addPartnerModal').modal('toggle');
+        $( "#addPartnerModal .close" ).click()
+    }
+    
+    downloadEmptyCsv(){
+        window.location.href = this.authenticationService.MEDIA_URL+"UPLOAD_USER_LIST _EMPTY.csv";
+    }
+    
     ngOnInit() {
         this.loggedInUserId = this.authenticationService.getUserId();
         this.defaultPartnerList( this.loggedInUserId );
