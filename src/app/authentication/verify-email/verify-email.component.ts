ca5953e4ab5c3b32029294aebf2a3d47195afa23
nareshcharykotha <knaresh@xtremand.com>
2018-01-25 10:21:35 +0530

    minor issues resolved in authentication module.

--- src/app/authentication/verify-email/verify-email.component.ts
+++ src/app/authentication/verify-email/verify-email.component.ts
@@ -2,30 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from '../../core/services/user.service';
 
-@Component({
-  selector: 'app-verify-email',
-  templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.css']
+@Component( {
+    selector: 'app-verify-email',
+    templateUrl: './verify-email.component.html',
+    styleUrls: ['./verify-email.component.css']
 })
 export class VerifyEmailComponent implements OnInit {
-public alias: string;
-constructor(private activatedRoute: ActivatedRoute, private userService:UserService, private router : Router) { }
+    public alias: string;
+    constructor( private activatedRoute: ActivatedRoute, private userService: UserService, private router: Router ) { }
 
-activateAccount(){
-    this.userService.activateAccount(this.alias)
-    .subscribe(
-     (result: any) => {
-      console.log(result);
-      this.router.navigate( ['/login'] );
-});
-}
+    activateAccount() {
+        this.userService.activateAccount( this.alias )
+            .subscribe(
+            ( result: any ) => {
+                console.log( result );
+                this.router.navigate( ['/login'] );
+            });
+    }
 
-ngOnInit() {
-    this.activatedRoute.queryParams.subscribe(
-        (param: any) => {
-            this.alias = param['alias'];
-        });
-    this.activateAccount();
-}
+    ngOnInit() {
+        this.activatedRoute.queryParams.subscribe(
+            ( param: any ) => {
+                this.alias = param['alias'];
+            });
+        this.activateAccount();
+    }
 
 }
