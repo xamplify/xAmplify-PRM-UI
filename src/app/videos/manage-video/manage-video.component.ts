1f9b8fc374887f2f03d416bd2d55a0f6a3ea38c7
KothaSathish <sathish7chary@gmail.com>
2018-01-29 20:57:01 +0530

    ui issue fixed in my profile page

--- src/app/videos/manage-video/manage-video.component.ts
+++ src/app/videos/manage-video/manage-video.component.ts
@@ -326,8 +326,8 @@ export class ManageVideoComponent implements OnInit, OnDestroy {
             text: 'You wont be able to revert this!',
             type: 'warning',
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
+            swalConfirmButtonColor: '#54a7e9',
+            swalCancelButtonColor: '#999',
             confirmButtonText: 'Yes, delete it!'
         }).then(function (myData: any) {
             console.log('ManageVidoes showAlert then()' + myData);
