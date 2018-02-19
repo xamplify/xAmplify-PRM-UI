cb9037affcf3f54f61f44d98ed60673a89e82ce5
nareshcharykotha <knaresh@xtremand.com>
2018-02-19 16:26:33 +0530

    colors changed for showing profile info.

--- src/app/common/user-info/user-info.component.ts
+++ src/app/common/user-info/user-info.component.ts
@@ -1,35 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 declare var $: any;
 
-@Component({
-  selector: 'app-user-info',
-  templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.css']
+@Component( {
+    selector: 'app-user-info',
+    templateUrl: './user-info.component.html',
+    styleUrls: ['./user-info.component.css']
 })
 export class UserInfoComponent implements OnInit {
-@Input() userInfo: any;
-backgroudColor:any;
-  constructor() { }
+    @Input() userInfo: any;
+    backgroudColor: any;
+    constructor() { }
 
-  ngOnInit() {
-      if(this.userInfo.emailId){
-          let first = this.userInfo.emailId.charAt(0);
-          if(first == 'a' || first == 'b' || first == 'c' || first == 'd' || first == 'e' ){
-              this.backgroudColor = "#512da8";
-          }
-          else if(first == 'f' || first == 'g' || first == 'h' || first == 'i' || first == 'j' ){
-              this.backgroudColor = "#5d4037";
-          }
-          else if(first == 'k' || first == 'l' || first == 'm' || first == 'n' || first == 'o' ){
-              this.backgroudColor = "#ef6c00";
-          }
-          else if(first == 'p' || first == 'q' || first == 'r' || first == 's' || first == 't' ){
-              this.backgroudColor = "#01579b";
-          }
-          else if(first == 'u' || first == 'v' || first == 'w' || first == 'x' || first == 'y' || first == 'z'){
-              this.backgroudColor = "#26a69a";
-          }
-       }
-  }
+    ngOnInit() {
+        if ( this.userInfo.emailId ) {
+            let first = this.userInfo.emailId.charAt( 0 );
+            if ( first == 'a' || first == 'f' || first == 'k' || first == 'p' || first == 'u' ) {
+                this.backgroudColor = "#512da8";
+            }
+            else if ( first == 'b' || first == 'g' || first == 'l' || first == 'q' || first == 'v' ) {
+                this.backgroudColor = "#5d4037";
+            }
+            else if ( first == 'c' || first == 'h' || first == 'm' || first == 'r' || first == 'w' ) {
+                this.backgroudColor = "#ef6c00";
+            }
+            else if ( first == 'd' || first == 'i' || first == 'n' || first == 's' || first == 'x' ) {
+                this.backgroudColor = "#01579b";
+            }
+            else if ( first == 'e' || first == 'j' || first == 'o' || first == 't' || first == 'y' || first == 'z' ) {
+                this.backgroudColor = "#26a69a";
+            }
+        }
+    }
 
 }
