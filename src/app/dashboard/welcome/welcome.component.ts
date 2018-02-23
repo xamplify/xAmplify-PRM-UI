5b77e19c6ccf4c3ab8301e6532b4215d0a6cde1c
KothaSathish <sathish7chary@gmail.com>
2018-02-23 23:06:42 +0530

    welcome page router issue fixed

--- src/app/dashboard/welcome/welcome.component.ts
+++ src/app/dashboard/welcome/welcome.component.ts
@@ -8,7 +8,7 @@ import { DashboardService } from '../dashboard.service';
 import { UserDefaultPage } from '../../core/models/user-default-page';
 import { DashboardReport } from '../../core/models/dashboard-report';
 
-@Component( {
+@Component({
     selector: 'app-welcome',
     templateUrl: './welcome.component.html',
     styleUrls: ['./welcome.component.css']
@@ -24,66 +24,70 @@ export class WelcomeComponent implements OnInit {
     hasEmailTemplateRole = false;
     hasStatsRole = false;
     hasSocialStatusRole = false;
-    constructor( 
-            private userService: UserService, 
-            public authenticationService: AuthenticationService,
-            private referenceService: ReferenceService, 
-            private dashboardService: DashboardService 
-            ) {
+    constructor(
+        private userService: UserService,
+        public authenticationService: AuthenticationService,
+        private referenceService: ReferenceService,
+        private dashboardService: DashboardService
+    ) {
         this.dashboardReport = new DashboardReport();
         this.userDefaultPage = new UserDefaultPage();
         this.hasVideoRole = this.referenceService.hasRole(this.referenceService.roles.videRole);
         this.hasContactRole = this.referenceService.hasRole(this.referenceService.roles.contactsRole);
         this.hasCampaignRole = this.referenceService.hasRole(this.referenceService.roles.campaignRole);
-        this.hasEmailTemplateRole =  this.referenceService.hasRole(this.referenceService.roles.emailTemplateRole);
+        this.hasEmailTemplateRole = this.referenceService.hasRole(this.referenceService.roles.emailTemplateRole);
         this.hasStatsRole = this.referenceService.hasRole(this.referenceService.roles.statsRole);
         this.hasSocialStatusRole = this.referenceService.hasRole(this.referenceService.roles.socialShare);
 
     }
 
-    getDefaultPage( userId: number ) {
-        this.userService.getUserDefaultPage( userId )
+    getDefaultPage(userId: number) {
+        this.userService.getUserDefaultPage(userId)
             .subscribe(
-            data => {
-                if (data['_body'].includes('welcome')) {
-                    this.userDefaultPage.isCurrentPageDefaultPage = true;
-                    this.referenceService.userDefaultPage = 'WELCOME';
-                }
-            },
-            error => console.log( error ),
-            () => { }
+                data => {
+                    try {
+                        if (data === 'welcome' || data['_body'].includes('welcome')) {
+                            this.userDefaultPage.isCurrentPageDefaultPage = true;
+                            this.referenceService.userDefaultPage = 'WELCOME';
+                        }
+                    } catch (error) {
+                        console.log('data undefined in welcome page');
+                    }
+                },
+                error => console.log(error),
+                () => { }
             );
     }
-    setWelcomeAsDefaultPage( event: any ) {
-        this.referenceService.userDefaultPage = event ?  'WELCOME' : 'DASHBOARD';
+    setWelcomeAsDefaultPage(event: any) {
+        this.referenceService.userDefaultPage = event ? 'WELCOME' : 'DASHBOARD';
         this.userService.setUserDefaultPage(this.authenticationService.getUserId(), this.referenceService.userDefaultPage)
             .subscribe(
-            data => {
-                this.userDefaultPage.isCurrentPageDefaultPage = event;
-                this.userDefaultPage.responseType = 'SUCCESS';
-                this.userDefaultPage.responseMessage = 'Your setting has been saved successfully';
-            },
-            error => {
-                this.userDefaultPage.responseType = 'ERROR';
-                this.userDefaultPage.responseMessage = 'an error occurred while processing your request';
-            },
-            () => { }
+                data => {
+                    this.userDefaultPage.isCurrentPageDefaultPage = event;
+                    this.userDefaultPage.responseType = 'SUCCESS';
+                    this.userDefaultPage.responseMessage = 'Your setting has been saved successfully';
+                },
+                error => {
+                    this.userDefaultPage.responseType = 'ERROR';
+                    this.userDefaultPage.responseMessage = 'an error occurred while processing your request';
+                },
+                () => { }
             );
     }
 
     dashboardReports(loggedInUserId: number) {
         this.dashboardService.loadDashboardReportsCount(this.loggedInUserId)
             .subscribe(
-            data => {
-                this.dashboardReport.totalViews = data.totalVideoViewsCount;
-                this.dashboardReport.totalContacts = data.totalcontactsCount;
-                this.dashboardReport.totalUploadedvideos = data.totalVideosCount;
-                this.dashboardReport.toalEmailTemplates = data.totalEmailTemplatesCount;
-                this.dashboardReport.totalCreatedCampaigns = data.totalCampaignsCount;
-                this.dashboardReport.totalSocialAccounts = data.totalSocialConnectionsCount;
-            },
-            error => console.log(error),
-            () => console.log('dashboard reports counts completed')
+                data => {
+                    this.dashboardReport.totalViews = data.totalVideoViewsCount;
+                    this.dashboardReport.totalContacts = data.totalcontactsCount;
+                    this.dashboardReport.totalUploadedvideos = data.totalVideosCount;
+                    this.dashboardReport.toalEmailTemplates = data.totalEmailTemplatesCount;
+                    this.dashboardReport.totalCreatedCampaigns = data.totalCampaignsCount;
+                    this.dashboardReport.totalSocialAccounts = data.totalSocialConnectionsCount;
+                },
+                error => console.log(error),
+                () => console.log('dashboard reports counts completed')
             );
 
     }
