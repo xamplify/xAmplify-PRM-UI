1c0450ac6d7e68dc96bc56ea5035c0fe059054d7
KothaSathish <sathish7chary@gmail.com>
2018-03-22 11:51:01 +0530

    boolean condition removed in pagination component

--- src/app/common/pagination/pagination.component.ts
+++ src/app/common/pagination/pagination.component.ts
@@ -11,12 +11,11 @@ export class PaginationComponent implements OnInit {
   @Input() type: string;
   @Input() isDropdown: boolean;
   @Output() notifyParent: EventEmitter<any>;
-
   @Output() notifyParentDropDown: EventEmitter<any>;
 
   pageNumber: any;
-  numberPerPage = [ {'name':'10','value':10},  {'name':'20','value':20}, {'name':'30','value':30},
-    {'name':'All','value':0}];
+  numberPerPage = [{ 'name': '10', 'value': 10 }, { 'name': '20', 'value': 20 }, { 'name': '30', 'value': 30 },
+  { 'name': 'All', 'value': 0 }];
 
   constructor() {
     this.notifyParent = new EventEmitter<any>();
@@ -28,20 +27,15 @@ export class PaginationComponent implements OnInit {
     const obj = { 'page': page, 'type': this.type }
     this.notifyParent.emit(obj);
   }
-  selectedPageNumber(event){
+  selectedPageNumber(event) {
     this.pageNumber.value = event;
-    if(event === 0){  event = this.pagination.totalRecords; }
+    if (event === 0) { event = this.pagination.totalRecords; }
     this.pagination.maxResults = event;
     this.pagination.pageIndex = 1;
     this.notifyParentDropDown.emit(this.pagination);
   }
   ngOnInit() {
     console.log(this.type);
-    if(this.isDropdown){
-
-    } else {
-      this.isDropdown = false;
-    }
   }
 
 }
