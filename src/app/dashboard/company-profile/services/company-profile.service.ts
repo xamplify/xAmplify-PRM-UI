d73d58e5222be7b3cd3134f85c8975dac35fd0b7
csravan <csravan@Xtemand.stratapps.com>
2018-03-24 21:02:15 +0530

    Company Profile Changes Added

--- src/app/dashboard/company-profile/services/company-profile.service.ts
+++ src/app/dashboard/company-profile/services/company-profile.service.ts
@@ -46,6 +46,8 @@ export class CompanyProfileService {
         .map(this.extractData)
         .catch(this.handleError);
     }
+    
+
 
     private extractData( res: Response ) {
         let body = res.json();
