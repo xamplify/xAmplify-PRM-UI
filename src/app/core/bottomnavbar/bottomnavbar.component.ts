d7e1ec1b23e8447d5cdd0af09d20bfaa81bbeda9
KothaSathish <sathish7chary@gmail.com>
2018-03-24 19:21:31 +0530

    bottom component chagnes added

--- src/app/core/bottomnavbar/bottomnavbar.component.ts
+++ src/app/core/bottomnavbar/bottomnavbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService} from '../../core/services/authentication.service';
 
 declare var $: any;
 @Component({
@@ -11,14 +12,14 @@ export class BottomnavbarComponent implements OnInit {
 
   isEmailTemplate: boolean;
 
-  constructor(public router: Router) {
+  constructor(public router: Router, public authenticationService: AuthenticationService) {
     this.isEmailTemplate = this.router.url.includes('/home/emailtemplates/create') ? true : false;
   }
   scrollTop() {
     $('html,body').animate({ scrollTop: 0 }, 'slow');
   }
   onResize(event) {
-    if ((window.outerHeight - window.innerHeight) > 100) {
+    if (this.isEmailTemplate && (window.outerHeight - window.innerHeight) > 100) {
       this.isEmailTemplate = false;
     }
   }
