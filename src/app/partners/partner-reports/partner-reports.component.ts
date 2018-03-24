5ae8e8b3bb045c4aa7a53becb81875edcae91adb
KothaSathish <sathish7chary@gmail.com>
2018-03-24 19:22:26 +0530

    partner reports page added

---
+++ src/app/partners/partner-reports/partner-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partner-reports',
+  templateUrl: './partner-reports.component.html',
+  styleUrls: ['./partner-reports.component.css']
+})
+export class PartnerReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/partners/partners.module.ts
+++ src/app/partners/partners.module.ts
@@ -6,6 +6,7 @@ import { ManagePartnersComponent } from './manage-partners/manage-partners.compo
 import { SharedContactsModule } from '../shared/shared-contacts.module';
 import { CommonComponentModule } from '../common/common.module';
 import { AddPartnersComponent } from './add-partners/add-partners.component';
+import { PartnerReportsComponent } from './partner-reports/partner-reports.component';
 
 
 @NgModule({
@@ -13,6 +14,6 @@ import { AddPartnersComponent } from './add-partners/add-partners.component';
     CommonModule, PartnerRoutingModule, SharedModule, SharedContactsModule, CommonComponentModule
   ],
   providers: [],
-  declarations: [ManagePartnersComponent, AddPartnersComponent]
+  declarations: [ManagePartnersComponent, AddPartnersComponent, PartnerReportsComponent]
 })
 export class PartnersModule { }
