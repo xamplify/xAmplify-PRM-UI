4268c71e7e64e9affccf0d14eee1d08452a14810
KothaSathish <sathish7chary@gmail.com>
2018-03-24 20:59:17 +0530

    partner reports page added and names corrected in dashboard

--- src/app/partners/partner-reports/partner-reports.component.ts
+++ src/app/partners/partner-reports/partner-reports.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../core/services/authentication.service';
 
 @Component({
   selector: 'app-partner-reports',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partner-reports.component.css']
 })
 export class PartnerReportsComponent implements OnInit {
+  worldMapdataReport: any;
 
-  constructor() { }
+  constructor(public router: Router, public authenticationService: AuthenticationService) { }
 
+  gotoMange(){
+    this.router.navigateByUrl('/home/partners/manage');
+  }
+  clickWorldMapReports(event){
+    console.log(event);
+  }
   ngOnInit() {
+    this.worldMapdataReport = [];
   }
 
 }
