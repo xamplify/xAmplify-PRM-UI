730b2501c7b45e0e4252c35f3420168344c86bbf
nareshcharykotha <knaresh@xtremand.com>
2018-04-12 15:40:36 +0530

    select email template page wording changed.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   isDebugMode: true,
-  // CLIENT_URL : 'https://xamplify.io/',
-  // SERVER_URL: 'https://xtremand.com/',  // xamplify production server
+   //CLIENT_URL : 'https://xamplify.io/',
+   //SERVER_URL: 'https://xtremand.com/',  // xamplify production server
   
   CLIENT_URL : 'https://socialubuntu.com/',
   SERVER_URL: 'https://aravindu.com/'  // social ubuntu prodution testing 
