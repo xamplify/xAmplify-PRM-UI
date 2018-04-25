17fe96945f24bf8dc0460cdc763e7d7d4bd3b3b7
KothaSathish <sathish7chary@gmail.com>
2018-04-25 14:39:26 +0530

    back button url changed in topnav bar page

--- src/app/core/topnavbar/topnavbar.component.ts
+++ src/app/core/topnavbar/topnavbar.component.ts
@@ -94,6 +94,6 @@ export class TopnavbarComponent implements OnInit {
 
   logout() {
     this.authenticationService.logout();
-    this.router.navigate(['/login']);
+    this.router.navigate(['/']);
   }
 }
