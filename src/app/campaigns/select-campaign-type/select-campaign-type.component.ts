5b3c3d67e9286679398ed91568311876f1bafc47
KothaSathish <sathish7chary@gmail.com>
2018-05-09 22:19:22 +0530

    home router added in select campaign page

--- src/app/campaigns/select-campaign-type/select-campaign-type.component.ts
+++ src/app/campaigns/select-campaign-type/select-campaign-type.component.ts
@@ -13,16 +13,16 @@ declare var swal, $,  Metronic, Layout , Demo,TableManaged ,Promise;
     templateUrl: './select-campaign-type-component.html',
     styleUrls: ['../../../assets/css/pricing-table.css']
   })
-  
+
 export class SelectCampaignTypeComponent implements OnInit{
-   
+
     public emailTypes = ['Video Campaign','Regular Campaign'];
     roleName:Roles=new Roles();
     hasSocialStatusRole:boolean = false;
     isOrgAdmin:boolean = false;
     isSocialCampaignAccess = true;
     isOnlyPartner = false;
-    constructor(private route: ActivatedRoute, private fb: FormBuilder,private logger:XtremandLogger,private router:Router,private refService:ReferenceService,private authenticationService:AuthenticationService){
+    constructor(private route: ActivatedRoute, private fb: FormBuilder,private logger:XtremandLogger,private router:Router,public refService:ReferenceService,public authenticationService:AuthenticationService){
         this.logger.info("select-campaign-type constructor loaded");
         let roles = this.authenticationService.getRoles();
         if(roles.indexOf(this.roleName.socialShare)>-1|| roles.indexOf(this.roleName.allRole)>-1){
@@ -33,10 +33,10 @@ export class SelectCampaignTypeComponent implements OnInit{
         }
         this.isOnlyPartner  = this.authenticationService.isOnlyPartner();
     }
-   
+
     ngOnInit() {
         try{
-            Metronic.init(); 
+            Metronic.init();
             Layout.init();
             Demo.init();
             TableManaged.init();
@@ -44,10 +44,10 @@ export class SelectCampaignTypeComponent implements OnInit{
             this.logger.error("error in select-campaign-type ngOnInit()", error);
             swal( 'Oops...','Something went wrong!','error');
         }
-        
+
     }
-    
-    
+
+
     createRegularCampaign(){
          this.refService.selectedCampaignType = "regular";
          this.router.navigate(["/home/campaigns/create"]);
@@ -56,6 +56,6 @@ export class SelectCampaignTypeComponent implements OnInit{
          this.refService.selectedCampaignType = "video";
          this.router.navigate(["/home/campaigns/create"]);
      }
-    
-    
+
+
 }
