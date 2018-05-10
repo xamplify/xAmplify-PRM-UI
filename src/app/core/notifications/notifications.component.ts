a1eb7ead259cd27c291d47aa3a81b8262c1eda68
nareshcharykotha <knaresh@xtremand.com>
2018-05-10 14:32:21 +0530

    pagination dropdown added in notification page and delete button added last in manage contact page.

--- src/app/core/notifications/notifications.component.ts
+++ src/app/core/notifications/notifications.component.ts
@@ -4,23 +4,35 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from '../../core/services/user.service';
 import { AuthenticationService } from '../../core/services/authentication.service';
 import { ReferenceService } from '../services/reference.service';
+import { SocialPagerService } from '../../contacts/services/social-pager.service';
+import { PaginationComponent } from '../../common/pagination/pagination.component';
+
 
 @Component({
     selector: 'app-notifications',
     templateUrl: './notifications.component.html',
-    styleUrls: ['./notifications.component.css']
+    styleUrls: ['./notifications.component.css'],
+    providers: [ SocialPagerService, PaginationComponent ]
 })
 export class NotificationsComponent implements OnInit {
     @Input('isTopNavBar') isTopNavBar = false;
-    notifications: any;
+    notifications = [];
+    pager: any = {};
+    pagedItems: any[];
+    pageSize: number = 12;
+    pageNumber: any;
     constructor(private router: Router, public userService: UserService, public authenticationService: AuthenticationService,
-    public referenceService:ReferenceService) { }
+    public referenceService:ReferenceService, public socialPagerService: SocialPagerService, public paginationComponent: PaginationComponent) {
+        
+        this.pageNumber = this.paginationComponent.numberPerPage[0];
+    }
 
     listNotifications() {
         this.userService.listNotifications(this.authenticationService.getUserId())
             .subscribe(
             data => {
                 this.notifications = data;
+                this.setPage( 1 );
             },
             error => console.log(error),
             () => console.log('Finished')
@@ -57,6 +69,22 @@ export class NotificationsComponent implements OnInit {
         }
         this.router.navigate(['/home/' + notification.targetUrl]);
     }
+    
+    setPage( page: number ) {
+        if ( page < 1 || page > this.pager.totalPages ) {
+            return;
+        }
+        this.pager = this.socialPagerService.getPager( this.notifications.length, page, this.pageSize );
+        this.pagedItems = this.notifications.slice( this.pager.startIndex, this.pager.endIndex + 1 );
+    }
+    
+    selectedPageNumber(event) {
+        this.pageNumber.value = event;
+        if (event === 0) { event = this.notifications.length; }
+        this.pageSize = event;
+        this.setPage(1);
+      }
+    
     ngOnInit() {
         this.listNotifications();
     }
