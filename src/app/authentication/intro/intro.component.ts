ba3cd0c31dee654a219c1302129b42c178316540
smanas <smanas@DESKTOP-IIKE7VO.stratapps.com>
2018-05-14 20:48:01 +0530

    Intro page CSS changes done

--- src/app/authentication/intro/intro.component.ts
+++ src/app/authentication/intro/intro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
@@ -10,6 +11,18 @@ export class IntroComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    window.onscroll = function () { myFunction() };
+
+    const navbar = document.getElementById("navbar");
+    const sticky = navbar.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("stuck")
+      } else {
+        navbar.classList.remove("stuck");
+      }
+    }
   }
 
 }
