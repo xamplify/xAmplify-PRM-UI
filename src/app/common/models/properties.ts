d3c21a7d52f81f0665e1e6413d4732fcc6ef872a
ksanthosh <ksanthosh@LAP046.stratapps.com>
2018-05-16 15:30:46 +0530

    ui issue fixed in login component

--- src/app/common/models/properties.ts
+++ src/app/common/models/properties.ts
@@ -7,82 +7,82 @@ export class Properties {
     APP_WELCOME_MESSAGE = 'Welcome to Xamplify';
     APP_Xamplify_URL="https://xamplify.io"
 
-    NO_RESULTS_FOUND = "No results Found";
-    EMAIL_SENT_SUCCESS = "Email sent successfully";
-    NO_USERS_SELECT_ERROR = "You have not selected any user";
+    NO_RESULTS_FOUND = "No results Found.";
+    EMAIL_SENT_SUCCESS = "Email sent successfully.";
+    NO_USERS_SELECT_ERROR = "You have not selected any user.";
     //FOR CONTACTS
-    FILE_TYPE_ERROR = "File type not allowed please select the .csv file only"
-    CONTACT_LIST_CREATE_SUCCESS = "Your contact list has been created successfully";
-    CONTACT_LIST_CREATE_ERROR = "An error occured while creating your contact list";
+    FILE_TYPE_ERROR = "File type not allowed please select the .csv file only."
+    CONTACT_LIST_CREATE_SUCCESS = "Your contact list has been created successfully.";
+    CONTACT_LIST_CREATE_ERROR = "An error occured while creating your contact list.";
 
-    CONTACT_LIST_UPDATE_SUCCESS = "Your contact list has been updated successfully";
-    CONTACT_LIST_UPDATE_ERROR: "An error occured while updating your contact list";
+    CONTACT_LIST_UPDATE_SUCCESS = "Your contact list has been updated successfully.";
+    CONTACT_LIST_UPDATE_ERROR: "An error occured while updating your contact list.";
 
-    CONTACT_LIST_DELETE_SUCCESS = "Your contact list has been deleted successfully";
-    CONTACT_LIST_DELETE_ERROR = "An Error occured while deleting your Contact List";
+    CONTACT_LIST_DELETE_SUCCESS = "Your contact list has been deleted successfully.";
+    CONTACT_LIST_DELETE_ERROR = "An Error occured while deleting your Contact List.";
 
-    CONTACT_LIST_SYNCHRONIZATION_SUCCESS = "Your contact list has been synchronized successfully";
-    CONTACT_LIST_SYNCHRONIZATION_ERROR = "An error occured while synchronizing your Contact List";
+    CONTACT_LIST_SYNCHRONIZATION_SUCCESS = "Your contact list has been synchronized successfully.";
+    CONTACT_LIST_SYNCHRONIZATION_ERROR = "An error occured while synchronizing your Contact List.";
 
-    CONTACTS_CREATE_SUCCESS = "Your contacts list have been created successfully";
+    CONTACTS_CREATE_SUCCESS = "Your contacts list have been created successfully.";
     CONTACTS_CREATE_ERROR = "An error occured while creating your Contacts";
 
-    CONTACTS_UPDATE_SUCCESS = "Your contact has been updated successfully";
-    CONTACTS_UPDATE_ERROR = "An error occured while updating your contacts";
+    CONTACTS_UPDATE_SUCCESS = "Your contact has been updated successfully.";
+    CONTACTS_UPDATE_ERROR = "An error occured while updating your contacts.";
 
-    CONTACTS_DELETE_SUCCESS = "Your Contacts have been deleted successfully";
-    CONTACTS_DELETE_ERROR = "An error occured while deleting your Contacts";
+    CONTACTS_DELETE_SUCCESS = "Your Contacts have been deleted successfully.";
+    CONTACTS_DELETE_ERROR = "An error occured while deleting your contacts";
 
-    CONTACT_SAVE_SUCCESS = "Your contact list has been updated successfully";
-    CONTACT_SAVE_SUCCESS_AND_MAIL_SENT_SUCCESS = "Your contact list has been updated successfully and campaign mails has been sent successfully";
-    CONTACTS_SAVE_ERROR = "An error occured while saving your contacts";
+    CONTACT_SAVE_SUCCESS = "Your contact list has been updated successfully.";
+    CONTACT_SAVE_SUCCESS_AND_MAIL_SENT_SUCCESS = "Your contact list has been updated successfully and campaign mails has been sent successfully.";
+    CONTACTS_SAVE_ERROR = "An error occured while saving your contacts.";
 
-    CONTACT_LIST_NAME_UPDATE_SUCCESS = "Your contacts have been saved successfully";
-    CONTACT_LIST_NAME_UPDATE_ERROR = "An error occured while saving your contacts";
+    CONTACT_LIST_NAME_UPDATE_SUCCESS = "Your contacts have been saved successfully.";
+    CONTACT_LIST_NAME_UPDATE_ERROR = "An error occured while saving your contacts.";
 
-    SOCIAL_ACCOUNT_REMOVED_SUCCESS = "Your social account has been removed successfully";
-    SOCIAL_ACCOUNT_REMOVED_ERROR = "An error occured while removing your social Account";
+    SOCIAL_ACCOUNT_REMOVED_SUCCESS = "Your social account has been removed successfully.";
+    SOCIAL_ACCOUNT_REMOVED_ERROR = "An error occured while removing your social Account.";
 
     /* For Partners*/
 
-    PARTNER_LIST_SYNCHRONIZATION_SUCCESS = "Your Parntner List has been synchronized successfully";
-    PARTNER_LIST_SYNCHRONIZATION_ERROR = "An error occured while synchronizing your partner list";
+    PARTNER_LIST_SYNCHRONIZATION_SUCCESS = "Your Parntner List has been synchronized successfully.";
+    PARTNER_LIST_SYNCHRONIZATION_ERROR = "An error occured while synchronizing your partner list.";
 
-    PARTNER_SAVE_SUCCESS_AND_MAIL_SENT_SUCCESS = "Your Partner list has been updated successfully and campaign mails has been sent successfully";
+    PARTNER_SAVE_SUCCESS_AND_MAIL_SENT_SUCCESS = "Your Partner list has been updated successfully and campaign mails has been sent successfully.";
 
-    PARTNERS_CREATE_SUCCESS = "Your Partner List has been created successfully";
-    PARTNERS_CREATE_ERROR = "An error occured while creating your Partner";
+    PARTNERS_CREATE_SUCCESS = "Your Partner List has been created successfully.";
+    PARTNERS_CREATE_ERROR = "An error occured while creating your partner";
 
-    PARTNERS_UPDATE_SUCCESS = "Your Partner has been updated successfully";
-    PARTNERS_UPDATE_ERROR = "An error occured while updating your Partner";
+    PARTNERS_UPDATE_SUCCESS = "Your Partner has been updated successfully.";
+    PARTNERS_UPDATE_ERROR = "An error occured while updating your partner.";
 
-    PARTNERS_DELETE_SUCCESS = "Your Partners have been deleted successfully";
-    PARTNERS_DELETE_ERROR = "An error occured while deleting your Partner";
+    PARTNERS_DELETE_SUCCESS = "Your Partners have been deleted successfully.";
+    PARTNERS_DELETE_ERROR = "An error occured while deleting your partner.";
 
-    PARTNERS_SAVE_SUCCESS = "Your Partners have been saved successfully";
-    PARTNERS_SAVE_ERROR = "An error occured while saving your Partners";
+    PARTNERS_SAVE_SUCCESS = "Your Partners have been saved successfully.";
+    PARTNERS_SAVE_ERROR = "An error occured while saving your partners.";
 
     /* Authentication Module*/
 
-    FORGOT_PASSWORD_MAIL_SEND_SUCCESS = "Check your inbox for a	temporary password";
-    FORGOT_PASSWORD_MAIL_SEND_ERROR = "An error occured while sending mail";
+    FORGOT_PASSWORD_MAIL_SEND_SUCCESS = "Check your inbox for a	temporary password.";
+    FORGOT_PASSWORD_MAIL_SEND_ERROR = "An error occured while sending mail.";
 
     SIGN_UP_SUCCESS = "Thanks for signing up! Please check your inbox for our account activation email.";
-    ACCOUNT_ACTIVATED_SUCESS = 'Thanks for activating your account.Please login to complete your company profile';
-    SIGN_UP_ERROR = "An error occurred while processing your request. Please try after some time";
-    ACCOUNT_DEACTIVATE_SUCCESS = "OrgAdmin deactivation successfully done";
+    ACCOUNT_ACTIVATED_SUCESS = 'Thanks for activating your account. Please login to complete your company profile.';
+    SIGN_UP_ERROR = "An error occurred while processing your request. Please try after some time.";
+    ACCOUNT_DEACTIVATE_SUCCESS = "OrgAdmin deactivation successfully done.";
 
-    BAD_CREDENTIAL_ERROR = "Username or password is incorrect";
-    USER_ACCOUNT_ACTIVATION_ERROR = "Your account has not been activated. Please check your inbox for our activation email";
-    EMPTY_CREDENTIAL_ERROR = "Username or password can't be empty";
-    RESEND_ACTIVATION_MAIL = 'Activation mail has been sent to your inbox, please check your inbox'
+    BAD_CREDENTIAL_ERROR = "Username or password is incorrect.";
+    USER_ACCOUNT_ACTIVATION_ERROR = "Your account has not been activated. Please check your inbox for our activation email.";
+    EMPTY_CREDENTIAL_ERROR = "Username or password can't be empty.";
+    RESEND_ACTIVATION_MAIL = 'Activation mail has been sent to your inbox. Please check your inbox.'
     /* User */
     PROCESS_REQUEST_SUCCESS = "Your request has been successfully processed.";
     PROCESS_REQUEST_ERROR = "An error occurred while processing your request.";
-    DEFAULT_PLAYER_SETTINGS = 'Default player settings updated Successfully';
-    VIDEO_LOGO_UPDATED = 'Co-Branding video logo uploaded successfully';
-    PROFILE_PIC_UPDATED='Profile Picture Updated successfully';
-    PROFILE_UPDATED = 'Profile Updated successfully';
-    PASSWORD_UPDATED= 'Password Updated successfully';
-    PAWORD_ERROR = 'New Password should not be same as Current password'
+    DEFAULT_PLAYER_SETTINGS = 'Default player settings updated successfully.';
+    VIDEO_LOGO_UPDATED = 'Co-Branding video logo uploaded successfully.';
+    PROFILE_PIC_UPDATED='Profile Picture Updated successfully.';
+    PROFILE_UPDATED = 'Profile Updated successfully.';
+    PASSWORD_UPDATED= 'Password Updated successfully.';
+    PAWORD_ERROR = 'New Password should not be same as current password.'
 }
