42f577e796fbd55d66fd4b1dbadad2e886109eab
nareshcharykotha <knaresh@xtremand.com>
2018-05-23 16:30:36 +0530

    add mobile code number flag in my-profile page.

--- src/app/dashboard/user-profile/my-profile/my-profile.component.ts
+++ src/app/dashboard/user-profile/my-profile/my-profile.component.ts
@@ -462,7 +462,8 @@ export class MyProfileComponent implements OnInit, AfterViewInit, OnDestroy {
         this.updateUserProfileForm = this.fb.group({
             'firstName': [this.userData.firstName, Validators.compose([Validators.required, noWhiteSpaceValidator, Validators.maxLength(50)])],//Validators.pattern(nameRegEx)
             'lastName': [this.userData.lastName, Validators.compose([Validators.required, noWhiteSpaceValidator, Validators.maxLength(50)])],//Validators.pattern(nameRegEx)
-            'mobileNumber': [this.userData.mobileNumber, Validators.compose([Validators.minLength(10), Validators.maxLength(10), Validators.pattern(mobileNumberPatternRegEx)])],
+           // 'mobileNumber': [this.userData.mobileNumber, Validators.compose([Validators.minLength(10), Validators.maxLength(10), Validators.pattern(mobileNumberPatternRegEx)])],
+            'mobileNumber': [this.userData.mobileNumber],
             'interests': [this.userData.interests, Validators.compose([noWhiteSpaceValidator, Validators.maxLength(50)])],
             'occupation': [this.userData.occupation, Validators.compose([noWhiteSpaceValidator, Validators.maxLength(50)])],
             'description': [this.userData.description, Validators.compose([noWhiteSpaceValidator, Validators.maxLength(50)])],
