6b2312e24259ae2db40cc3e559b48815bfbe95e0
nareshcharykotha <knaresh@xtremand.com>
2018-06-18 22:45:05 +0530

    condition changed in vendor report page.

--- src/app/dashboard/vendor-reports/vendor-reports.component.ts
+++ src/app/dashboard/vendor-reports/vendor-reports.component.ts
@@ -29,7 +29,7 @@ export class VendorReportsComponent implements OnInit {
                   this.vendorDetails = data;
                   
                   this.pagination.totalRecords = data.totalRecords;
-                  this.pagination = this.pagerService.getPagedItems( this.pagination, this.vendorDetails );
+                  this.pagination = this.pagerService.getPagedItems( this.pagination, this.vendorDetails.data );
               },
               error => console.log(error),
               () => console.log('vendor reports completed')
