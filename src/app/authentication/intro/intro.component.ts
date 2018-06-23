abcb8babdc0c9e2d264a0ef56ca7bf93fb4125df
KothaSathish <sathish7chary@gmail.com>
2018-06-24 00:12:13 +0530

    scroll issue fixed in intro and terms condition page

--- src/app/authentication/intro/intro.component.ts
+++ src/app/authentication/intro/intro.component.ts
@@ -1,60 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../../core/services/authentication.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../../core/services/authentication.service";
 
 @Component({
-  selector: 'app-intro',
-  templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.css']
+  selector: "app-intro",
+  templateUrl: "./intro.component.html",
+  styleUrls: ["./intro.component.css"]
 })
 export class IntroComponent implements OnInit {
-  mainLoader:boolean;
-
-   // google maps zoom level
-   zoom  = 10;
-   // initial center position for the map
-   lat = 40.7143528;
-   lng = -74.0059731;
-   markers = [
+  mainLoader: boolean;
+  navbar: any;
+  sticky: any;
+  // google maps zoom level
+  zoom = 10;
+  // initial center position for the map
+  lat = 40.7143528;
+  lng = -74.0059731;
+  markers = [
     {
       lat: 40.7143528,
       lng: -74.0059731,
-      label: 'A',
+      label: "A",
       draggable: true
     }
-  ]
-   constructor(public authenticationService:AuthenticationService) { }
-   clickedMarker(label: string, index: number) {
-     console.log(`clicked the marker: ${label || index}`)
-   }
+  ];
+  constructor(public authenticationService: AuthenticationService) {}
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
+  }
 
-   mapClicked($event: MouseEvent) {
-    //  this.markers.push({
-    //    lat: $event.coords.lat,
-    //    lng: $event.coords.lng,
-    //    draggable: true
-    //  });
-   }
+  mapClicked($event: MouseEvent) {
+    //  this.markers.push({lat: $event.coords.lat, lng: $event.coords.lng, draggable: true });
+  }
 
-   markerDragEnd(m: any, $event: MouseEvent) {
-     console.log('dragEnd', m, $event);
-   }
+  markerDragEnd(m: any, $event: MouseEvent) {
+    console.log("dragEnd", m, $event);
+  }
+  onScroll() {
+    if (window.pageYOffset >= this.sticky) { this.navbar.classList.add("stuck");
+    } else { this.navbar.classList.remove("stuck"); }
+  }
   ngOnInit() {
+    this.navbar = document.getElementById("navbar");
+    this.sticky = this.navbar.offsetTop;
     this.mainLoader = true;
-    try{
-    if(localStorage.getItem('currentUser')){ this.authenticationService.navigateToDashboardIfUserExists();
-      setTimeout(()=>{  this.mainLoader = false;},900);
-     }
-    else {this.mainLoader=false; }
-    }catch(error){console.log('error'+error);}
-    window.onscroll = function () { myFunction() };
-    const navbar = document.getElementById("navbar");
-    const sticky = navbar.offsetTop;
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("stuck")
+    try {
+      if (localStorage.getItem("currentUser")) {
+        this.authenticationService.navigateToDashboardIfUserExists();
+        setTimeout(() => {
+          this.mainLoader = false;
+        }, 900);
       } else {
-        navbar.classList.remove("stuck");
+        this.mainLoader = false;
       }
+    } catch (error) {
+      console.log("error" + error);
+      this.mainLoader = true;
     }
   }
 }
--- src/app/authentication/terms-conditon/terms-conditon.component.ts
+++ src/app/authentication/terms-conditon/terms-conditon.component.ts
@@ -1,30 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-terms-conditon',
-  templateUrl: './terms-conditon.component.html',
-  styleUrls: ['./terms-conditon.component.css','../intro/intro.component.css']
+  selector: "app-terms-conditon",
+  templateUrl: "./terms-conditon.component.html",
+  styleUrls: ["./terms-conditon.component.css", "../intro/intro.component.css"]
 })
 export class TermsConditonComponent implements OnInit {
-  termsPage:boolean;
-  constructor(public router:Router) {
-    this.termsPage = this.router.url.includes('terms')? true:false;
-   }
-
-  ngOnInit() {
-    window.onscroll = function () { myFunction() };
-
-    const navbar = document.getElementById("navbar");
-    const sticky = navbar.offsetTop;
-
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("stuck")
-      } else {
-        navbar.classList.remove("stuck");
-      }
-    }
+  termsPage: boolean;
+  navbar: any;
+  sticky: any;
+  constructor(public router: Router) {
+    this.termsPage = this.router.url.includes("terms") ? true : false;
   }
-
+  onScroll() {
+    if (window.pageYOffset >= this.sticky) { this.navbar.classList.add("stuck");
+    } else { this.navbar.classList.remove("stuck"); }
+  }
+  ngOnInit() {
+    this.navbar = document.getElementById("navbar");
+    this.sticky = this.navbar.offsetTop;
+  }
 }
