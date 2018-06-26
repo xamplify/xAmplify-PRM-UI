5af38866c9d8d1917d083067377bcc5dff6cad7b
KothaSathish <sathish7chary@gmail.com>
2018-06-26 10:53:54 +0530

    tooltip message ui changes

--- src/app/videos/manage-video/manage-video.component.ts
+++ src/app/videos/manage-video/manage-video.component.ts
@@ -265,7 +265,7 @@ export class ManageVideoComponent implements OnInit, OnDestroy {
                 this.pagination.pagedItems.splice(position, 1);
                 this.defaultBannerMessageValues();
                 this.showVideoFileName = videoName;
-                this.showVideoFileName = '"'+this.showVideoFileName+'" video deleted successfully';
+                this.showVideoFileName = '"'+this.showVideoFileName+'" deleted successfully';
                 this.customResponse = new CustomResponse( 'SUCCESS', this.showVideoFileName, true );
                 $('html,body').animate({ scrollTop: 0 }, 'slow');
                 if (this.pagination.pagedItems.length === 0) {
@@ -348,11 +348,11 @@ export class ManageVideoComponent implements OnInit, OnDestroy {
             this.showVideoFileName = videoFile.title;
         }
         if(this.showMessage){
-            this.showVideoFileName = '"'+this.showVideoFileName+'" video saved successfully';
+            this.showVideoFileName = '"'+this.showVideoFileName+'" saved successfully';
             this.customResponse = new CustomResponse( 'SUCCESS', this.showVideoFileName, true );
 
          } else if(this.showUpdatevalue){
-            this.showVideoFileName = '"'+this.showVideoFileName+'" video settings updated successfully';
+            this.showVideoFileName = '"'+this.showVideoFileName+'" settings updated successfully';
             this.customResponse = new CustomResponse( 'SUCCESS', this.showVideoFileName, true );
          }
         this.xtremandLogger.info('update method called ' + this.showVideoFileName);
