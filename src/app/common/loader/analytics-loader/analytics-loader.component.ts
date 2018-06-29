1733f955f5b7b3a9d647d9012fe9e5f54b4ce589
KothaSathish <sathish7chary@gmail.com>
2018-06-29 16:13:18 +0530

    code formatted in analytics loader and removed unused variables

--- src/app/common/loader/analytics-loader/analytics-loader.component.ts
+++ src/app/common/loader/analytics-loader/analytics-loader.component.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { ReferenceService } from '../../../core/services/reference.service';
-
+import { Component, OnInit } from "@angular/core";
+import { ReferenceService } from "../../../core/services/reference.service";
 
 @Component({
-  selector: 'app-analytics-loader',
-  templateUrl: './analytics-loader.component.html',
-  styleUrls: ['./analytics-loader.component.css','../list-loader/list-loader.component.css']
+  selector: "app-analytics-loader",
+  templateUrl: "./analytics-loader.component.html",
+  styleUrls: [ "./analytics-loader.component.css",  "../list-loader/list-loader.component.css" ]
 })
 export class AnalyticsLoaderComponent implements OnInit {
-  campaignType:any;
   rowsCount = [0, 1, 2, 3];
-  textLoader = [0,1,2];
-  constructor(private referenceService:ReferenceService) {
-  }
+  textLoader = [0, 1, 2];
 
-  ngOnInit() {
-    this.campaignType = this.referenceService.campaignType;
-  }
+  constructor(public referenceService: ReferenceService) {}
 
+  ngOnInit() {}
 }
