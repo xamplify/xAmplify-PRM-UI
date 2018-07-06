9c1d38b66108bc5f4c79caa7271719d8cf248a48
KothaSathish <sathish7chary@gmail.com>
2018-07-06 10:33:07 +0530

    environment file changes for prod and qa and dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
   isDebugMode: true,
-   CLIENT_URL : 'https://xamplify.io/', // xamplify client app server
-   SERVER_URL : 'https://xamp.io/',  // xamplify backend production server
-  
-  // CLIENT_URL : 'https://socialubuntu.com/', //socail ubuntu client app server
-  // SERVER_URL: 'https://aravindu.com/'  // social ubuntu backend prodution testing 
+  CLIENT_URL: 'https://xamplify.io/', // xamplify client app server
+  SERVER_URL: 'https://xamp.io/' // xamplify backend production server
 };
