38e0410ee8d5802e3cdd4c2d12c0429b06e66143
KothaSathish <sathish7chary@gmail.com>
2018-07-16 11:27:19 +0530

    Partner email added in properties file E2E test files

--- e2e/authentication/properties.ts
+++ e2e/authentication/properties.ts
@@ -1,5 +1,6 @@
 export const Properties = {
-  userName: "ksathish@stratapps.com",
+  userName: "ksathish@stratapps.com", // vendor with partner
+ // userName: "sathish7chary@gmail.com", partner account
   password: "Sathish@123",
   forgotEmail: 'sathishcharykotha@gmail.com'
 };
