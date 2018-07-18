25da7d174c756ec32e9ebb311dc4236d5b592579
KothaSathish <sathish7chary@gmail.com>
2018-07-18 14:41:08 +0530

    Change to Video Based Reports > Reports - video statistics and data names changed according to dropdowns

--- protractor.conf.js
+++ protractor.conf.js
@@ -27,7 +27,7 @@ exports.config = {
   },
   onPrepare() {
     jasmine.getEnv().addReporter(
-      new SpecReporter({ spec: { displayStacktrace: true } }),
+      new SpecReporter({ spec: { displayStacktrace: true } })
     //  new Jasmine2HtmlReporter({ savePath: './results/', screenshotsFolder: 'images',})
     );
     browser.manage().window().setSize(1600, 1000);
