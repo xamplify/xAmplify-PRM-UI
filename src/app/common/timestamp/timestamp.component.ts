8760f63a11727a1da822afe3ccfab05aed5abccf
KothaSathish <sathish7chary@gmail.com>
2018-08-05 18:37:30 +0530

    date pipe issue fixed in safari browser

--- src/app/common/timestamp/timestamp.component.ts
+++ src/app/common/timestamp/timestamp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ReferenceService } from '../../core/services/reference.service';
 
 @Component({
   selector: 'app-timestamp',
@@ -9,10 +10,22 @@ export class TimestampComponent implements OnInit {
  @Input() dateAndTime: any;
  @Input() isOnlyTime = false;
  @Input() isOnlyDate = false;
-    
-  constructor() { }
+ isSpacethere = false;
+
+  constructor(public referenceService: ReferenceService) { }
 
   ngOnInit() {
+    try{
+      if(this.referenceService.isSafariBrowser()){
+      if((this.dateAndTime.includes(' '))) {
+        console.log(this.dateAndTime);
+        this.isSpacethere = true;
+        // this.dateAndTime = this.dateAndTime.replace(/ /g,"T");
+        // this.dateAndTime = Date.parse(this.dateAndTime);
+      }else {
+      }
+    }
+    }catch(error){ console.log(error);}
   }
 
 }
