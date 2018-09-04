2b7f4e7310b0bd335b4de05ec6873952aea7d362
smanas <smanas@DESKTOP-IIKE7VO.stratapps.com>
2018-09-04 19:17:55 +0530

    Created separate componenet for tweet and embeded the app-tweet component in tweets page and profile page.

---
+++ src/app/social/twitter/tweet/tweet.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.css']
+})
+export class TweetComponent implements OnInit {
+@Input('tweet') tweet: any;
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
