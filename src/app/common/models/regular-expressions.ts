8acdc2e674e895948bc6113fe711a9c9acd5d0cd
nareshcharykotha <knaresh@xtremand.com>
2018-09-21 12:41:31 +0530

    Password regular expression chnaged for accepting all special symbols.

--- src/app/common/models/regular-expressions.ts
+++ src/app/common/models/regular-expressions.ts
@@ -1,6 +1,6 @@
 export class RegularExpressions {
     EMAIL_ID_PATTERN = /^(([a-zA-Z0-9.!#$&'*+\/=?_`{|}~-]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    PASSWORD_PATTERN = '((?=.*\\d)(?=.*[A-Za-z])(?=.*[@#$%^&+=!]).{6,20})';
+    PASSWORD_PATTERN = '((?=.*\\d)(?=.*[A-Za-z])(?=.*[^a-zA-Z0-9]).{6,20})';
     CITY_PATTERN = /[a-zA-Z]+[a-zA-Z ]+/;
     ADDRESS_PATTERN = /^[a-zA-Z0-9-\/] ?([a-zA-Z0-9-\/]|[a-zA-Z0-9-\/] )*[a-zA-Z0-9-\/]$/;
     PHONE_NUMBER_PATTERN = /^[0-9-+]+$/;
