53a0dc1cad6b3cd01ea8ccd0a035a5bfa3e133ae
KothaSathish <sathish7chary@gmail.com>
2018-10-05 16:52:55 +0530

    preview video ui changes

--- src/app/common/preview-video/preview-video.component.ts
+++ src/app/common/preview-video/preview-video.component.ts
@@ -86,6 +86,7 @@ export class PreviewVideoComponent implements OnInit, OnDestroy {
             $( "#videoId" ).css( "width", "550px" );
             $( "#videoId" ).css( "height", "310px" );
             $( "#videoId" ).css( "max-width", "100%" );
+            $( "#videoId" ).css( "margin", "0 auto" );
         } else {
             console.log( "Loaded Normal Video" );
             $( '.p-video' ).remove();
@@ -99,6 +100,7 @@ export class PreviewVideoComponent implements OnInit, OnDestroy {
             $( "#videoId" ).css( "width", "550px" );
             $( "#videoId" ).css( "height", "310px" );
             $( "#videoId" ).css( "max-width", "100%" );
+            $( "#videoId" ).css( "margin", "0 auto" );
             var document: any = window.document;
             const overrideNativeValue = this.referenceService.getBrowserInfoForNativeSet();
             this.videoJSplayer = videojs( "videoId", {
