6c2f510ef8066d1f30eb83485656aaddc4f0c87a
KothaSathish <sathish7chary@gmail.com>
2018-10-16 16:26:37 +0530

    welcome image folder added and release environment file created

---
+++ src/environments/environment.release.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  isDebugMode: true,
+  CLIENT_URL: 'https://release.xamplify.io/', // xamplify client app server
+  SERVER_URL: 'https://release.xamp.io/' // xamplify backend production server
+
+};
