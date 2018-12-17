89361eefeea1256685dfeb6d6f6b54a7b65ba541
KothaSathish <sathish7chary@gmail.com>
2018-12-17 18:06:39 +0530

    select campaign hiding and showing campaign types client side changes

--- src/app/campaigns/select-campaign-type/select-campaign-type.component.ts
+++ src/app/campaigns/select-campaign-type/select-campaign-type.component.ts
@@ -4,6 +4,9 @@ import { XtremandLogger } from '../../error-pages/xtremand-logger.service';
 import { ReferenceService } from '../../core/services/reference.service';
 import { AuthenticationService } from '../../core/services/authentication.service';
 import { Roles } from '../../core/models/roles';
+import { CampaignService } from '../services/campaign.service';
+import { UserService } from 'app/core/services/user.service';
+
 declare var Metronic, Layout , Demo,TableManaged:any;
 @Component({
     selector: 'app-select-campaign',
@@ -19,7 +22,14 @@ export class SelectCampaignTypeComponent implements OnInit{
     isOrgAdmin = false;
     isSocialCampaignAccess = true;
     isOnlyPartner = false;
-    constructor(private logger:XtremandLogger,private router:Router,public refService:ReferenceService,public authenticationService:AuthenticationService){
+    changeClass = 'col-xs-12';
+    videoCampaign = false;
+    emailCampaign = false;
+    socialCampaign = false;
+    eventCampaign = false;
+
+    constructor(private logger:XtremandLogger,private router:Router,public refService:ReferenceService,public authenticationService:AuthenticationService,
+      public campaignService: CampaignService, public userService:UserService){
         this.logger.info("select-campaign-type constructor loaded");
         let roles = this.authenticationService.getRoles();
         if(roles.indexOf(this.roleName.socialShare)>-1|| roles.indexOf(this.roleName.allRole)>-1){
@@ -30,9 +40,40 @@ export class SelectCampaignTypeComponent implements OnInit{
         }
         this.isOnlyPartner = this.authenticationService.isOnlyPartner();
     }
+    cssClassChange(){
+      const countOfTrues = [this.videoCampaign,this.emailCampaign, this.socialCampaign, this.eventCampaign].filter(Boolean).length;
+      if(countOfTrues === 4) { this.changeClass = 'col-xs-12 col-sm-3';
+      } else if (countOfTrues === 3) { this.changeClass = 'col-xs-12 col-sm-4';
+      } else if (countOfTrues === 2) { this.changeClass = 'col-xs-12 col-sm-6';
+      } else if (countOfTrues === 1) { this.changeClass = 'col-xs-12'; }
+
+    }
+    getOrgCampaignTypes(){
+      this.campaignService.getOrgCampaignTypes( this.refService.companyId).subscribe(
+      data=>{
+        console.log(data);
+        this.videoCampaign = data.video;
+        this.emailCampaign = data.regular;
+        this.socialCampaign = data.social;
+        this.eventCampaign = data.event
+        this.cssClassChange();
+      });
+    }
+    getCompanyId() {
+      try {
+        this.userService.getVideoDefaultSettings().subscribe(
+          (result: any) => {
+            if (result !== "") {  this.refService.companyId = result.companyProfile.id;
+              this.getOrgCampaignTypes();
+            }
+          }, (error: any) => { console.log(error); }
+        );
+      } catch (error) { console.log(error);  } }
 
     ngOnInit() {
         try{
+            this.getCompanyId();
+            this.cssClassChange();
             Metronic.init();
             Layout.init();
             Demo.init();
--- src/app/campaigns/services/campaign.service.ts
+++ src/app/campaigns/services/campaign.service.ts
@@ -514,4 +514,9 @@ export class CampaignService {
             .map(this.extractData)
             .catch(this.handleError);
     }
+    getOrgCampaignTypes(companyId: any) {
+      return this.http.get(this.URL + `campaign/access/${companyId}?access_token=${this.authenticationService.access_token}` )
+          .map(this.extractData)
+          .catch(this.handleError);
+  }
 }
