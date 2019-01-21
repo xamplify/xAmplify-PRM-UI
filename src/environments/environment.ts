c31a0f0baf091ad2ffca7976ee152964ab53cf43
Manas Sahoo <smanas@stratapps.com>
2019-01-21 14:41:19 +0530

    Added comments for conflict.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,5 +19,6 @@ export const environment = {
   // CLIENT_URL : 'https://xamplify.io/', // xamplify client app server
   SERVER_URL: "https://aravindu.com/"
   //SERVER_URL: 'https://release.xamp.io/'
-  //SERVER_URL: 'http://localhost:8080/'
+  // LOCAL_SERVER_URL: 'http://localhost:8080/'
+  // SERVER_URL: 'http://localhost:8080/'
 };
