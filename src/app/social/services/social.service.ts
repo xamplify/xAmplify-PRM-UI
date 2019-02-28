a68ce817b707b65f0580f7ce88e745e86dbbc0ef
manas-xtremand <smanas@stratapps.com>
2019-02-28 19:40:05 +0530

    handleError() method code refactored in social service class

--- src/app/social/services/social.service.ts
+++ src/app/social/services/social.service.ts
@@ -191,9 +191,21 @@ export class SocialService {
     return body || {};
   }
 
-  private handleError(error: any) {
-    const errMsg = (error.message) ? error.message :
-      error.status ? `${error.status} - ${error.statusText}` : 'Server   error';
-    return Observable.throw(errMsg);
-  }
+    private handleError( error: any ) {
+        const body = error['_body'];
+        if ( body !== "" ) {
+            var response = JSON.parse( body );
+            if ( response.message != undefined ) {
+                return Observable.throw( response.message );
+            } else {
+                return Observable.throw( response.error );
+            }
+
+        } else {
+            let errMsg = ( error.message ) ? error.message :
+                error.status ? `${error.status} - ${error.statusText}` : 'Server   error';
+            return Observable.throw( error );
+        }
+
+    }
 }
