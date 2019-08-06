bc993b8d2d2a10a20c2cad60537ddb8ed14aa16c
nareshcharykotha <knaresh@xtremand.com>
2019-08-06 11:40:00 +0530

    issue number #1024 fixed.

--- src/app/dashboard/dashboard-stats/dashboard-stats.component.ts
+++ src/app/dashboard/dashboard-stats/dashboard-stats.component.ts
@@ -42,12 +42,12 @@ export class DashboardStatsComponent implements OnInit {
   }
 
   navigateToManageContacts() {
-    if (this.authenticationService.isVendor() || this.authenticationService.isSuperAdmin()) {
+    if (this.authenticationService.isVendor() || this.authenticationService.isSuperAdmin() && this.dashboardReport.totalTeamMembers != 0 ) {
       this.router.navigate(["/home/team/add-team"]);
-    } else if (this.authenticationService.isAddedByVendor && !this.authenticationService.isSuperAdmin()) {
+    } else if (this.authenticationService.isAddedByVendor && !this.authenticationService.isSuperAdmin() && this.dashboardReport.totalTeamMembers != 0 ) {
       // this.router.navigate(['/home/partners/manage']);
       this.router.navigate(["/home/team/add-team"]);
-    } else if (!this.authenticationService.isVendor() && !this.authenticationService.isSuperAdmin() && this.dashboardReport.totalContacts != 0) {
+    } else if (!this.authenticationService.isVendor() && !this.authenticationService.isSuperAdmin() && this.dashboardReport.totalContacts != 0 ) {
       this.router.navigate(["/home/contacts/manage"]);
     }
   }
