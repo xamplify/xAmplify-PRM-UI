df8ec7f85500b5f0f48325f6ae7bc40c84e149d1
lvivek <lvivek@LAP050.stratapps.com>
2019-12-23 16:47:17 +0530

    SF Changes

--- src/app/deal-registration/sf-deal/sf-deal.component.ts
+++ src/app/deal-registration/sf-deal/sf-deal.component.ts
@@ -4,6 +4,7 @@ import { Form } from 'app/forms/models/form';
 import { ColumnInfo } from 'app/forms/models/column-info';
 import { ReferenceService } from 'app/core/services/reference.service';
 import { FormOption } from 'app/forms/models/form-option';
+import { Campaign } from 'app/campaigns/models/campaign';
 
 declare var flatpickr: any, $: any;
 
@@ -15,6 +16,7 @@ declare var flatpickr: any, $: any;
 export class SfDealComponent implements OnInit {
 
   @Input() dealId: any;
+  @Input() campaign: any;
   form: Form = new Form();
   errorMessage: string;
   isDealRegistrationFormValid: boolean = true;
@@ -25,6 +27,9 @@ export class SfDealComponent implements OnInit {
   ngOnInit() {
     this.contactService.displaySfForm(this.dealId).subscribe(result => {
       this.form = result.data;
+      if(this.campaign.campaignName !== undefined || this.campaign.campaignName !== ''){
+        this.form.formLabelDTOs.find(field => field.labelId === 'Name').value = this.campaign.campaignName;
+      }      
       let reqFieldsCheck = this.form.formLabelDTOs.filter(column => column.required && (column.value === undefined || column.value === ""));
       if (reqFieldsCheck.length === 0) {
         this.isDealRegistrationFormValid = false;
