53e8a565004fe09645d66002f06f6a5b1718161e
lvivek <lvivek@LAP050.stratapps.com>
2019-12-27 11:26:04 +0530

    Added Validations for Salesforce Custom Form

--- src/app/deal-registration/sf-deal/sf-deal.component.ts
+++ src/app/deal-registration/sf-deal/sf-deal.component.ts
@@ -4,9 +4,8 @@ import { Form } from 'app/forms/models/form';
 import { ColumnInfo } from 'app/forms/models/column-info';
 import { ReferenceService } from 'app/core/services/reference.service';
 import { FormOption } from 'app/forms/models/form-option';
-import { Campaign } from 'app/campaigns/models/campaign';
 
-declare var flatpickr: any, $: any;
+declare var $: any;
 
 @Component({
   selector: 'app-sf-deal',
@@ -20,16 +19,15 @@ export class SfDealComponent implements OnInit {
   form: Form = new Form();
   errorMessage: string;
   isDealRegistrationFormValid: boolean = true;
-  isRequiredColumns: any;
-
-  constructor(private contactService: ContactService, private referenceService: ReferenceService) { }
+  constructor(private contactService: ContactService, private referenceService: ReferenceService) {
+  }
 
   ngOnInit() {
     this.contactService.displaySfForm(this.dealId).subscribe(result => {
       this.form = result.data;
-      if(this.campaign.campaignName !== undefined || this.campaign.campaignName !== ''){
+      if (this.campaign.campaignName !== undefined || this.campaign.campaignName !== '') {
         this.form.formLabelDTOs.find(field => field.labelId === 'Name').value = this.campaign.campaignName;
-      }      
+      }
       let reqFieldsCheck = this.form.formLabelDTOs.filter(column => column.required && (column.value === undefined || column.value === ""));
       if (reqFieldsCheck.length === 0) {
         this.isDealRegistrationFormValid = false;
@@ -39,16 +37,7 @@ export class SfDealComponent implements OnInit {
     });
   }
 
-  validateField(columnInfo: ColumnInfo) {
-
-    if (columnInfo.labelType === 'email') {
-      if (!this.referenceService.validateEmailId($.trim(columnInfo.value))) {
-        columnInfo.errorMessage = "Please enter a valid email address";
-        columnInfo.divClass = "error";
-      } else {
-        columnInfo.divClass = "success";
-      }
-    }
+  validateField(columnInfo: ColumnInfo) {    
     this.validateRequiredFields();
   }
 
@@ -59,7 +48,6 @@ export class SfDealComponent implements OnInit {
     if (event.target.checked) {
       columnInfo.value = "true";
     } else {
-      //columnInfo.value.splice($.inArray(formOption.id, columnInfo.value), 1);
       columnInfo.value = "false";
     }
     this.validateRequiredFields();
@@ -72,9 +60,46 @@ export class SfDealComponent implements OnInit {
   validateRequiredFields() {
     let reqFieldsCheck = this.form.formLabelDTOs.filter(column => column.required && (column.value === undefined || column.value === ""));
     if (reqFieldsCheck.length === 0) {
-      this.isDealRegistrationFormValid = false;
+      this.isDealRegistrationFormValid = false;      
     } else {
       this.isDealRegistrationFormValid = true;
+    }
+    if(!this.isDealRegistrationFormValid){
+      let allEmails = this.form.formLabelDTOs.filter(column => column.labelType === "email");
+      for (let emailObj of allEmails) {
+        if(!this.isDealRegistrationFormValid){
+          this.validateEmailId(emailObj);
+        }        
+      }
+      let allPercentages = this.form.formLabelDTOs.filter(column => column.labelType === "percent");
+      for (let percentObj of allPercentages) {
+        if(!this.isDealRegistrationFormValid){
+          this.validatePercentage(percentObj);
+        }        
+      }
+    }
+  }
+
+  validateEmailId(columnInfo: ColumnInfo) {
+    if (!this.referenceService.validateEmailId($.trim(columnInfo.value))) {
+      columnInfo.errorMessage = "Please enter a valid email address";
+      columnInfo.divClass = "error";
+      this.isDealRegistrationFormValid = true;
+    } else {
+      columnInfo.divClass = "success";
+    }
+  }
+
+  validatePercentage(columnInfo: ColumnInfo) {
+    if (columnInfo.value !== null) {
+      var x = parseFloat($.trim(columnInfo.value));
+      if (isNaN(x) || x < 0 || x > 100) {
+        columnInfo.errorMessage = "Please enter a value between 0 and 100";
+        columnInfo.divClass = "error";
+        this.isDealRegistrationFormValid = true;
+      } else {
+        columnInfo.divClass = "success";
+      }
     }
   }
 }
