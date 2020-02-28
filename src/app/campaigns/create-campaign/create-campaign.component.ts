76df7df12a2eafee097ac8f8d581e125b8091d86
csravan <csravan@LAP060.stratapps.com>
2020-02-29 02:38:08 +0530

    Email notification is enabled for to partner page campaign

--- src/app/campaigns/create-campaign/create-campaign.component.ts
+++ src/app/campaigns/create-campaign/create-campaign.component.ts
@@ -306,6 +306,9 @@ export class CreateCampaignComponent implements OnInit,OnDestroy{
             this.setActiveTabForVideo();
             this.campaign.countryId = this.countries[0].id;
             this.onSelect(this.campaign.countryId);
+            if(this.campaignType=='landingPage'){
+                this.campaign.emailNotification = true;
+            }
         }
         if ( this.authenticationService.user != undefined ) {
             this.loggedInUserId = this.authenticationService.getUserId();
