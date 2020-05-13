3062397e87df2f6b347f11770c6b4fcd0af0fde2
csravan <csravan@LAP060.stratapps.com>
2020-05-13 13:42:03 +0530

    Ng idle implementation is completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,32 @@ import { AuthenticationService } from "./core/services/authentication.service";
 import { Title }     from '@angular/platform-browser';
 
 import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+
+import { Idle, DEFAULT_INTERRUPTSOURCES, IdleExpiry, LocalStorageExpiry } from '@ng-idle/core';
+import { Keepalive } from '@ng-idle/keepalive';
+
+
 declare var QuickSidebar, $: any;
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [LocalStorageExpiry, { provide: IdleExpiry, useExisting: LocalStorageExpiry }, Idle],
+
 })
 export class AppComponent implements OnInit, AfterViewInit {
 
-    constructor(private titleService: Title,public userService: UserService,public authenticationService: AuthenticationService, public env: EnvService, private slimLoadingBarService: SlimLoadingBarService, private router: Router) {
-        // logger.level = logger.Level.LOG;
+  idleState = 'Not started.';
+  timedOut = false;
+  lastPing?: Date = null;
+  title = 'angular-idle-timeout';
+sessionExpireMessage = "Your session has timed out. Please login again.";
+    constructor(private idle: Idle, private keepalive: Keepalive, private titleService: Title,public userService: UserService,public authenticationService: AuthenticationService, public env: EnvService, private slimLoadingBarService: SlimLoadingBarService, private router: Router) {
+      
+      //this.checkIdleState(idle,keepalive);
     }
-    
+
     getTeamMembersDetails(){
         this.userService.getRoles(this.authenticationService.getUserId())
         .subscribe(
@@ -37,20 +50,8 @@ export class AppComponent implements OnInit, AfterViewInit {
     
     
     ngOnInit() {
-
-        window.addEventListener('storage', (event) => {
-            if (event.storageArea == localStorage) {
-                const currentUser = localStorage.getItem( 'currentUser' );
-              if(currentUser==undefined){
-                this.authenticationService.sessinExpriedMessage = "Your session has timed out. Please login again.";
-                this.authenticationService.logout();
-              }else{
-                this.authenticationService.sessinExpriedMessage = "";
-              }
-             
-            }
-          }, false);
-
+      console.log("on inint");
+        this.logoutFromAllBrowsers();
         //QuickSidebar.init();
        // this.getTeamMembersDetails();
         // reloading the same url with in the application
@@ -76,4 +77,77 @@ export class AppComponent implements OnInit, AfterViewInit {
           $('body').tooltip({ selector: '[data-toggle="tooltip"]' }); 
           $('body').popover({ selector: '[data-toggle="popover"]' }); 
     }
+
+    logoutFromAllBrowsers(){
+      window.addEventListener('storage', (event) => {
+          if (event.storageArea == localStorage) {
+              const currentUser = localStorage.getItem( 'currentUser' );
+            if(currentUser==undefined){
+              this.authenticationService.sessinExpriedMessage = this.sessionExpireMessage;
+              this.authenticationService.logout();
+            }else{
+              this.authenticationService.sessinExpriedMessage = "";
+            }
+           
+          }
+        }, false);
+    }
+
+
+    /***************Idle State Check************* */
+    checkIdleState(idle:Idle,keepalive:Keepalive){
+      // sets an idle timeout of 5*60*60 seconds, for testing purposes.
+      idle.setIdle(5);
+      // sets a timeout period of 5 seconds. after 15 seconds of inactivity, the user will be considered timed out.
+      idle.setTimeout(5);
+      // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
+      idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+  
+      idle.onIdleEnd.subscribe(() => { 
+        this.idleState = 'No longer idle.'
+        console.log(this.idleState);
+        this.resetIdleState();
+      });
+      
+      idle.onTimeout.subscribe(() => {
+        this.idleState = 'Timed out!';
+        this.timedOut = true;
+        console.log(idle);
+        console.log(this.idleState);
+        this.authenticationService.sessinExpriedMessage = this.sessionExpireMessage;
+        this.authenticationService.logout();
+      });
+      
+      idle.onIdleStart.subscribe(() => {
+          this.idleState = 'You\'ve gone idle!'
+          console.log(this.idleState);
+      });
+      
+      idle.onTimeoutWarning.subscribe((countdown) => {
+        this.idleState = 'You will time out in ' + countdown + ' seconds!'
+      });
+  
+      // sets the ping interval to 15 seconds
+      keepalive.interval(1800);
+      keepalive.onPing.subscribe(() => this.lastPing = new Date());
+      this.authenticationService.getUserLoggedIn().subscribe(userLoggedIn => {
+        if (userLoggedIn) {
+          idle.watch();
+          this.timedOut = false;
+        } else {
+          console.log(this.lastPing);
+          console.log("timeout");
+          idle.stop();
+        }
+      });
+      }
+  
+      resetIdleState() {
+        this.idle.watch();
+        //xthis.idleState = 'Started.';
+        this.timedOut = false;
+      }
+
+
+ 
 }
