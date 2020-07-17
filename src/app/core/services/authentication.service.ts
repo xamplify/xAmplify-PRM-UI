a22e127816987a78d3a528aeb2d4b7efe548ec77
csravan <csravan@LAP060.stratapps.com>
2020-07-17 13:44:48 +0530

    Company logo and footer is added in form preview popup

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -88,8 +88,12 @@ export class AuthenticationService {
     this.SERVER_URL = this.envService.SERVER_URL;
     this.APP_URL = this.envService.CLIENT_URL;
     this.REST_URL = this.SERVER_URL + 'xtremand-rest/';
-
-    this.MEDIA_URL = this.SERVER_URL + 'vod/';
+    if(this.SERVER_URL.indexOf('localhost')>-1){
+      this.MEDIA_URL = 'https://aravindu.com/vod/';
+    }else{
+      this.MEDIA_URL = this.SERVER_URL + 'vod/';
+    }
+    
     this.SHARE_URL = this.SERVER_URL + 'embed/';
 
     this.clientId = this.envService.clientId;
