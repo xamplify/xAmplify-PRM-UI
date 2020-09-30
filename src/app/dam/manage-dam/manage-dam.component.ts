269fc0b85ec2258b144a60efb0ba02ecb8178b1e
csravan <csravan@LAP060.stratapps.com>
2020-09-30 12:32:12 +0530

    Form alias css changes are added for partner landing page

--- src/app/dam/manage-dam/manage-dam.component.ts
+++ src/app/dam/manage-dam/manage-dam.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DamService } from '../services/dam.service';
+/*****Common Imports**********************/
+import { AuthenticationService } from '../../core/services/authentication.service';
+import { XtremandLogger } from "../../error-pages/xtremand-logger.service";
+import { ReferenceService } from "app/core/services/reference.service";
+import { Router } from '@angular/router';
+import { SortOption } from '../../core/models/sort-option';
+import { HttpRequestLoader } from '../../core/models/http-request-loader';
+import { UtilService } from '../../core/services/util.service';
+import { CustomResponse } from 'app/common/models/custom-response';
+import { Properties } from '../../common/models/properties';
+import { Pagination } from 'app/core/models/pagination';
+import { PagerService } from 'app/core/services/pager.service';
+import { ErrorResponse } from 'app/util/models/error-response';
+declare var $: any;
 @Component({
   selector: 'app-manage-dam',
   templateUrl: './manage-dam.component.html',
-  styleUrls: ['./manage-dam.component.css']
+  styleUrls: ['./manage-dam.component.css'],
+  providers: [HttpRequestLoader, SortOption, Properties]
 })
 export class ManageDamComponent implements OnInit {
-
-  constructor() { }
+  loading = false;
+  loggedInUserId:number = 0;
+  constructor(private utilService: UtilService, public sortOption: SortOption, public partnerListLoader: HttpRequestLoader, private damService: DamService, private pagerService: PagerService, public authenticationService: AuthenticationService, public xtremandLogger: XtremandLogger, public referenceService: ReferenceService, private router: Router, public properties: Properties) {
+    this.loggedInUserId = this.authenticationService.getUserId();
+  }
 
   ngOnInit() {
-	alert("MAnage");
   }
 
 }
