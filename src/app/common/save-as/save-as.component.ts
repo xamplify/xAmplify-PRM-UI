b7ca99599f067529969def4851fba244d1a3bb41
ksanthosh <ksanthosh@LAP046.stratapps.com>
2020-12-03 20:39:45 +0530

    code changes for save as in assign leads

--- src/app/common/save-as/save-as.component.ts
+++ src/app/common/save-as/save-as.component.ts
@@ -6,6 +6,11 @@ import { LegalBasisOption } from '../../dashboard/models/legal-basis-option';
 import { CallActionSwitch } from '../../videos/models/call-action-switch';
 import { ContactService } from '../../contacts/services/contact.service';
 import { Router } from '@angular/router';
+import { UserUserListWrapper } from '../../contacts/models/user-userlist-wrapper';
+import { User } from '../../core/models/user';
+import { ContactList } from '../../contacts/models/contact-list';
+import { AuthenticationService } from '../../core/services/authentication.service';
+
 
 declare var $:any;
 
@@ -34,11 +39,14 @@ export class SaveAsComponent implements OnInit {
   selectedLegalBasisOptions = [];
   isAssignLeads = false;
   module = '';
+  contactListObject: ContactList;
+  userUserListWrapper: UserUserListWrapper ;
+  loading = false;
 
 
 
   constructor(public referenceService:ReferenceService, public editContactsComponent:EditContactsComponent, public xtremandLogger:XtremandLogger,
-		  public callActionSwitch: CallActionSwitch, public contactService: ContactService, private router:Router,) {
+		  public callActionSwitch: CallActionSwitch, public contactService: ContactService, private router:Router, public authenticationService : AuthenticationService) {
     this.notifyParentSaveAs = new EventEmitter();
    this.model.isPublic = this.contactService.publicList;
 
@@ -86,23 +94,100 @@ export class SaveAsComponent implements OnInit {
   saveAsLeadsInputChecking() {
       try {
           const name = this.saveAsListName;
-          this.isValidLegalOptions = true;
+          this.validateLegalBasisOptions();
           const inputName = name.toLowerCase().replace(/\s/g, '');
           if (name !== "" && name.length < 250) {
-             // this.saveAsError = '';
-              this.validateLegalBasisOptions();
+              this.editContactsComponent.validateLegalBasisOptions();
               if (this.isValidLegalOptions) {
-                  this.editContactsComponent.saveDuplicateLeadList(this.saveAsListName, this.selectedLegalBasisOptions, this.model.isPublic);
-                  $('#saveAsModal').modal('hide');
-                  this.notifyParentSaveAs.emit('success');
+                  this.saveDuplicateLeadList(this.saveAsListName, this.selectedLegalBasisOptions, this.model.isPublic);
               }
           }
           else if (name == "") { this.saveAsError = 'List Name is Required.'; }
           else { this.saveAsError = 'You have exceeded 250 characters!'; }
       } catch (error) {
+          $('#saveAsModal').modal('hide');
           this.xtremandLogger.error(error, "EditContactsComponent", "saveAsLeadsInputChecking()");
       }
   }
+  
+  saveDuplicateLeadList(name: string, selectedLegalBasisOptions: any, isPublic: boolean) {
+      try {
+          if (name != "") {
+              this.userUserListWrapper = new UserUserListWrapper;
+              this.contactListObject = new ContactList;
+              this.contactListObject.name = name;
+              this.contactListObject.isPartnerUserList = this.isPartner;
+              this.contactListObject.contactType = 'ASSIGNED_LEADS_LIST';
+              this.contactListObject.publicList = true;
+              this.contactListObject.socialNetwork = 'MANUAL';
+              if (this.editContactsComponent.selectedContactListIds.length == 0) {
+                  let listUsers = [];
+                  listUsers = this.editContactsComponent.totalListUsers;
+                  $.each(listUsers, function(index, value: User) {
+                      value.legalBasis = selectedLegalBasisOptions;
+                  });
+                  console.log(listUsers);
+                  this.userUserListWrapper.users = listUsers;
+                  this.userUserListWrapper.userList = this.contactListObject;
+                  this.saveAssignedLeadsList(this.userUserListWrapper);
+              } else {
+                  for (let i = 0; i < this.editContactsComponent.selectedContactListIds.length; i++) {
+                      for (let j = 0; j < this.editContactsComponent.totalListUsers.length; j++) {
+                          if (this.editContactsComponent.selectedContactListIds[i] == this.editContactsComponent.totalListUsers[j].id) {
+                              this.editContactsComponent.totalListUsers[j].legalBasis = selectedLegalBasisOptions;
+                              this.editContactsComponent.selectedContactForSave.push(this.editContactsComponent.totalListUsers[j]);
+                              break;
+                          }
+                      }
+                  }
+                  console.log(this.editContactsComponent.selectedContactForSave);
+                  this.userUserListWrapper.users = this.editContactsComponent.selectedContactForSave;
+                  this.userUserListWrapper.userList = this.contactListObject;
+                  this.saveAssignedLeadsList(this.userUserListWrapper);
+              }
+          }
+          else {
+              this.xtremandLogger.error("AllContactComponent saveSelectedUsers() UserNotSelectedContacts");
+          }
+      } catch (error) {
+          this.xtremandLogger.error(error, "editContactComponent", "SaveAsNewList()");
+      }
+  }
+
+  saveAssignedLeadsList(userUserListWrapper: UserUserListWrapper) {
+      this.loading = true;
+      this.contactService.saveAssignedLeadsList(this.userUserListWrapper)
+          .subscribe(
+          data => {
+              if (data.access) {
+                  data = data;
+                  this.loading = false;
+                  if (data.statusCode === 401) {
+                      this.saveAsError = data.message;
+                  } else if (data.statusCode === 402) {
+                      this.saveAsError = data.message;
+                  } else {
+                      $('#saveAsModal').modal('hide');
+                      this.saveAsError = '';
+                      this.saveAsListName = '';
+                      this.saveAsListName = undefined;
+                      this.router.navigateByUrl('/home/assignleads/manage')
+                      this.contactService.saveAsSuccessMessage = "SUCCESS";
+                      this.contactService.isLoadingList = false;
+                  }
+              } else {
+                  this.authenticationService.forceToLogout();
+                  localStorage.removeItem('isZohoSynchronization');
+              }
+          },
+          (error: any) => {
+              this.loading = false;
+              $('#saveAsModal').modal('hide');
+              this.xtremandLogger.error(error);
+          },
+          () => this.xtremandLogger.info("ManageContactsComponent saveAssignedLeadsList() finished")
+          )
+      }
 
   updateListType(){
 	    try{
