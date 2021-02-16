f8d590eff9826f68764da35df100248eda8b7fcf
csravan-stratapps <csravan@stratapps.com>
2021-02-16 23:34:05 +0530

    Tree map is implemented for contacts

---
+++ src/app/dashboard/dashboard-analytics-components/contacts-statistics/contacts-statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-statistics',
+  templateUrl: './contacts-statistics.component.html',
+  styleUrls: ['./contacts-statistics.component.css']
+})
+export class ContactsStatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/dashboard/dashboard-analytics-components/partners-statistics/partners-statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partners-statistics',
+  templateUrl: './partners-statistics.component.html',
+  styleUrls: ['./partners-statistics.component.css']
+})
+export class PartnersStatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
