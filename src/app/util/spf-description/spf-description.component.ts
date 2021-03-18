0def24b285e78e1ad4eceee8fa2f436f8e3d332f
csravan-stratapps <csravan@stratapps.com>
2021-03-18 23:42:33 +0530

    SPF text is updated

---
+++ src/app/util/spf-description/spf-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spf-description',
+  templateUrl: './spf-description.component.html',
+  styleUrls: ['./spf-description.component.css']
+})
+export class SpfDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
