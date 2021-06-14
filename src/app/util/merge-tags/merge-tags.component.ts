6b538d86bfd1870497120ec30f990e57b3f34a99
csravan-stratapps <csravan@stratapps.com>
2021-06-14 09:50:11 +0530

    Unsubscribe link merge tag is hidden for event templates (XNFR-58)

--- src/app/util/merge-tags/merge-tags.component.ts
+++ src/app/util/merge-tags/merge-tags.component.ts
@@ -62,8 +62,10 @@ export class MergeTagsComponent implements OnInit {
 		if (this.isCampaign == undefined || !this.isCampaign) {
 			this.mergeTags.push({ name: 'Sender Company Contact Number', value: this.senderMergeTag.senderCompanyContactNumber });
 			this.mergeTags.push({ name: 'Sender About Us (Partner)', value: this.senderMergeTag.aboutUs });
-		    this.mergeTags.push({ name: 'Privacy Policy', value: this.senderMergeTag.privacyPolicy });
-		    this.mergeTags.push({ name: 'Unsubscribe Link', value: this.senderMergeTag.unsubscribeLink });
+			this.mergeTags.push({ name: 'Privacy Policy', value: this.senderMergeTag.privacyPolicy });
+			if(!this.isEvent){
+				this.mergeTags.push({ name: 'Unsubscribe Link', value: this.senderMergeTag.unsubscribeLink });
+			}
 		}
 		
 		if (this.isEvent) {
