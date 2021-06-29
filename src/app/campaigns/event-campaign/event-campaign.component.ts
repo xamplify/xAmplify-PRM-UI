c0102fc64667470b07f33211b21c22e45c26b064
ksanthosh <ksanthosh@LAP046.stratapps.com>
2021-06-29 21:41:05 +0530

    code changes for event campaign

--- src/app/campaigns/event-campaign/event-campaign.component.ts
+++ src/app/campaigns/event-campaign/event-campaign.component.ts
@@ -754,6 +754,7 @@ export class EventCampaignComponent implements OnInit, OnDestroy, AfterViewInit,
         if (this.isEditCampaign) {
             contactListsPagination.editCampaign = true;
             contactListsPagination.campaignId = this.eventCampaign.id;
+            contactListsPagination.channelCampaign = this.eventCampaign.channelCampaign ;
         }
         if (this.authenticationService.isOrgAdmin() || this.authenticationService.isOrgAdminPartner() || (!this.authenticationService.isAddedByVendor && !this.isVendor)) {
             this.contactListsPagination.filterValue = false;
@@ -852,6 +853,7 @@ export class EventCampaignComponent implements OnInit, OnDestroy, AfterViewInit,
         this.eventCampaign.channelCampaign = !this.eventCampaign.channelCampaign;
         this.contactListsPagination.channelCampaign = this.eventCampaign.channelCampaign;
         this.contactListsPagination.pageIndex = 1;
+        this.showContactType = true;
         if (!this.eventCampaign.channelCampaign) {
             this.eventCampaign.enableCoBrandingLogo = false;
             this.emailTemplatesPagination.emailTemplateType = EmailTemplateType.NONE;
