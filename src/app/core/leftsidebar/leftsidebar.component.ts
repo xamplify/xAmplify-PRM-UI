74c8a2f2c7074756891a8f1a2b64a0f1a8163034
pswathi <pswathi@stratapps.com>
2021-09-23 18:55:20 +0530

    UI is updated for PRM

--- src/app/core/leftsidebar/leftsidebar.component.ts
+++ src/app/core/leftsidebar/leftsidebar.component.ts
@@ -26,6 +26,7 @@ export class LeftsidebarComponent implements OnInit, DoCheck {
 	menuItem: MenuItem = new MenuItem();
 	menuItemError = false;
 	contentDivs: Array<boolean> = new Array<boolean>();
+	prmContentDivs: Array<boolean> = new Array<boolean>();
 	isSuperAdmin = false;
 	roleName: Roles = new Roles();
 	emailtemplates: boolean;
@@ -191,11 +192,15 @@ export class LeftsidebarComponent implements OnInit, DoCheck {
 			this.contentDivs.push(module.damAccess || module.damAccessAsPartner);
 			this.contentDivs.push(module.lmsAccess || module.lmsAccessAsPartner);
 			this.contentDivs.push(module.playbookAccess || module.playbookAccessAsPartner);
-			this.contentDivs.push();
 			const count = this.contentDivs.filter((value) => value).length;
 			module.contentDivsCount = count;
+			this.prmContentDivs.push(module.damAccess || module.damAccessAsPartner);
+			this.prmContentDivs.push(module.lmsAccess || module.lmsAccessAsPartner);
+			this.prmContentDivs.push(module.playbookAccess || module.playbookAccessAsPartner);
+			module.prmContentDivsCount = this.prmContentDivs.filter((value) => value).length;
 		} else {
 			module.contentDivsCount = 0;
+			module.prmContentDivsCount = 0;
 		}
 	}
 
--- src/app/core/models/module.ts
+++ src/app/core/models/module.ts
@@ -62,4 +62,5 @@ export class Module {
     allBoundSamlSettings = false;
 
     notifyPartners = false;
+    prmContentDivsCount = 0;
 }
