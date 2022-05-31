5c50d3496313e4bae0525ff8e8a208005327007e
csravan-stratapps <csravan@stratapps.com>
2022-05-31 12:08:03 +0530

    XNFR-127
    Code is refactored for donut pie chart

--- src/app/dashboard/dashboard-analytics-components/donut-pie-chart/donut-pie-chart.component.ts
+++ src/app/dashboard/dashboard-analytics-components/donut-pie-chart/donut-pie-chart.component.ts
@@ -1,27 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Properties } from 'app/common/models/properties';
-import { HttpRequestLoader } from 'app/core/models/http-request-loader';
-import { AuthenticationService } from 'app/core/services/authentication.service';
-import { DashboardService } from 'app/dashboard/dashboard.service';
-import { XtremandLogger } from 'app/error-pages/xtremand-logger.service';
-import { VanityLoginDto } from 'app/util/models/vanity-login-dto';
+import { Component, Input, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Properties } from "app/common/models/properties";
+import { AuthenticationService } from "app/core/services/authentication.service";
+import { DashboardService } from "app/dashboard/dashboard.service";
+import { XtremandLogger } from "app/error-pages/xtremand-logger.service";
+import { VanityLoginDto } from "app/util/models/vanity-login-dto";
 declare var Highcharts: any;
 
 @Component({
-  selector: 'app-donut-pie-chart',
-  templateUrl: './donut-pie-chart.component.html',
-  styleUrls: ['./donut-pie-chart.component.css'],
-  providers: [ Properties,HttpRequestLoader]
+  selector: "app-donut-pie-chart",
+  templateUrl: "./donut-pie-chart.component.html",
+  styleUrls: ["./donut-pie-chart.component.css"],
+  providers: [Properties],
 })
 export class DonutPieChartComponent implements OnInit {
   loader = false;
   statusCode = 200;
-  donutData:Array<any>=[]
+  donutData: Array<any> = [];
   @Input() applyFilter: boolean;
   vanityLoginDto: VanityLoginDto = new VanityLoginDto();
   loggedInUserId: number = 0;
-  val:any;
+  val: any;
   vanityLogin = false;
   constructor(
     public authenticationService: AuthenticationService,
@@ -44,74 +43,60 @@ export class DonutPieChartComponent implements OnInit {
     this.findDonutChart();
   }
 
-  findDonutChart(){
+  findDonutChart() {
     this.loader = true;
-    this.dashboardService.findActivePartnersAndInActivePartnersForDonutChart(this.vanityLoginDto)
-    .subscribe(
-      (response) => {
-    this.donutData =response.data;
-    this.val=this.donutData.map(t=>t[1]);
-    let sum = this.val.reduce(function (a, b) {
-    return a + b;
-    }, 0);
-    if(sum === 0){
-      this.donutData.length = 0;
-      this.loader =false;
-    }
-    else{
-    this.loader =false;
-    this.statusCode =200;
-    this.loadDonutChart(this.donutData);
-    }
-
-  },
-  (error) => {
-    this.xtremandLogger.error(error);
-    this.loader = false;
-    this.statusCode = 0;
+    this.dashboardService
+      .findActivePartnersAndInActivePartnersForDonutChart(this.vanityLoginDto)
+      .subscribe(
+        (response) => {
+          this.statusCode = response.statusCode;
+          this.donutData = response.data;
+          if (this.statusCode == 200) {
+            this.loadDonutChart(this.donutData);
+          }
+          this.loader = false;
+        },
+        (error) => {
+          this.xtremandLogger.error(error);
+          this.loader = false;
+          this.statusCode = 500;
+        }
+      );
   }
-  );
-}
 
-
-loadDonutChart(donutData :any){
-  this.loader =false;
-  this.statusCode =200;
-
-  Highcharts.chart('donut-chart-container', {
-    chart: {
-        type: 'pie',
+  loadDonutChart(donutData: any) {
+    Highcharts.chart("donut-chart-container", {
+      chart: {
+        type: "pie",
         options3d: {
-            enabled: false,
-        }
-    },
-    title: {
-        text: ''
-    },
-    subtitle: {
-        text: ''
-    },
-    legend: {
-      reversed: true
-  },
-    plotOptions: {
+          enabled: false,
+        },
+      },
+      title: {
+        text: "",
+      },
+      subtitle: {
+        text: "",
+      },
+      legend: {
+        reversed: true,
+      },
+      plotOptions: {
         pie: {
-            innerSize: 70,
-            depth: 10
-        }
-    },
-    credits: {
-                enabled: false
-            },
-    colors: [
-              "#E87E04",
-              "#8a8282c4"
-            ],
-    series: [{
-        name: 'Count',
-        data: this.donutData
-    }]
-});
-}
-
+          innerSize: 70,
+          depth: 10,
+        },
+      },
+      credits: {
+        enabled: false,
+      },
+      colors: ["#E87E04", "#8a8282c4"],
+      series: [
+        {
+          name: "Count",
+          data: this.donutData,
+        },
+      ],
+    });
+  }
 }
