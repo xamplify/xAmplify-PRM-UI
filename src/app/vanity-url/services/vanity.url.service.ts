bd4a215bcaf978c2c74c574ad6ddb1aa0842c950
csravan-stratapps <csravan@stratapps.com>
2022-06-20 15:51:36 +0530

    XNFR-130
    Content dashboard is changed for prm

--- src/app/vanity-url/services/vanity.url.service.ts
+++ src/app/vanity-url/services/vanity.url.service.ts
@@ -100,8 +100,8 @@ export class VanityURLService {
     //let url = "TGAInfoSolutions.xamplify.com";
     //let url = "analytify.xamplify.com";
     //  let url = "tga.xamplify.com";
-   // let url = "vendor.xamplify.com";
-    let url =window.location.hostname;
+    let url = "movva.xamplify.com";
+   // let url =window.location.hostname;
     //let url="JAVG.xamplify.com";
         if (!url.includes("release") && !url.includes("192.168")) {
       let domainName = url.split('.');
