ea2ca001e1f380df142724b0e7086dd486ab6a13
csravan-stratapps <csravan@stratapps.com>
2022-06-29 16:26:36 +0530

    colspan value updated for share leads list

--- src/app/util/one-click-launch-partner-preview/one-click-launch-partner-preview.component.ts
+++ src/app/util/one-click-launch-partner-preview/one-click-launch-partner-preview.component.ts
@@ -41,6 +41,7 @@ export class OneClickLaunchPartnerPreviewComponent implements OnInit {
   historyList:Array<any> = new Array<any>();
   historyResponse: CustomResponse = new CustomResponse();
   campaignPartnerId = 0;
+  colspanValue = 2;
   constructor(public authenticationService:AuthenticationService,public campaignService:CampaignService,public referenceService:ReferenceService,public properties:Properties,
     public contactService:ContactService,public pagerService:PagerService,public xtremandLogger:XtremandLogger) { }
 
@@ -110,6 +111,11 @@ viewShareLeads(partner:any){
     pagination.parentCampaignId = this.campaignId;
 		pagination.campaignId = this.campaignId;
 		this.showExpandButton = $.trim(pagination.searchKey).length>0;
+    if(this.showExpandButton){
+			this.colspanValue= 3;
+		}else{
+			this.colspanValue = 2;
+		}
 		this.contactService.loadAssignedLeadsLists(pagination).
 		subscribe(
 			(data:any)=>{
