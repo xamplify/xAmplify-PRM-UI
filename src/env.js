7c579eaa417f971aed566a029b2efc518a307973
csravan-stratapps <csravan@stratapps.com>
2022-07-03 19:45:38 +0530

    blur and logout loader is added

--- src/env.js
+++ src/env.js
@@ -22,7 +22,7 @@
 
     /*********************QA**********************/
     // window.__env.CLIENT_URL = 'https://xamplify.co/';
-   // window.__env.SERVER_URL =  'https://aravindu.com/';
+    //window.__env.SERVER_URL =  'https://aravindu.com/';
    //  window.__env.imagesHost = "https://aravindu.com/vod/images/";
 
 
