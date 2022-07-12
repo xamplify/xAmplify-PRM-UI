e8540ba2c5950421cbad8365a57759d2e4838393
csravan-stratapps <csravan@stratapps.com>
2022-07-12 12:38:02 +0530

    FindAll Agency Url Updated (XNFR-83)

--- src/app/agency/services/agency.service.ts
+++ src/app/agency/services/agency.service.ts
@@ -9,12 +9,16 @@ export class AgencyService {
   ACCESS_TOKEN_SUFFIX_URL = "?access_token="+this.authenticationService.access_token;
   AGENCY_PREFIX_URL = this.authenticationService.REST_URL + "agency";
   AGENCY_URL = this.AGENCY_PREFIX_URL+this.ACCESS_TOKEN_SUFFIX_URL;
-  FIND_ALL_AGENCIES_URL = this.AGENCY_PREFIX_URL+"/findAll"+this.ACCESS_TOKEN_SUFFIX_URL;
   constructor(private http: Http, private authenticationService: AuthenticationService, private logger: XtremandLogger) { }
 
   /***Get All Agencies****/
   findAll(pagination:Pagination){
-    return this.http.post(this.FIND_ALL_AGENCIES_URL,pagination)
+    let page = pagination.pageIndex;
+    let size = pagination.maxResults;
+    let searchKey = pagination.searchKey;
+    let sort = pagination.sortcolumn+","+pagination.sortingOrder;
+    let findAllUrl = this.AGENCY_URL+"&page="+page+"&size="+size+"&sort="+sort+"&search="+searchKey;
+    return this.http.get(findAllUrl,"")
           .map(this.authenticationService.extractData)
           .catch(this.authenticationService.handleError);
   }
