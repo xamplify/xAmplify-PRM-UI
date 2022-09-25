edd656d4275551353079686f301532dc7868c88f
csravan-stratapps <csravan@stratapps.com>
2022-09-25 11:41:53 +0530

    Move Devices API added

--- src/app/azuga/devices-info/devices-info.component.ts
+++ src/app/azuga/devices-info/devices-info.component.ts
@@ -5,6 +5,7 @@ import { AzugaService } from '../service/azuga.service';
 import { XtremandLogger } from 'app/error-pages/xtremand-logger.service';
 import { HttpRequestLoader } from 'app/core/models/http-request-loader';
 import { Properties } from 'app/common/models/properties';
+import { CustomResponse } from 'app/common/models/custom-response';
 declare var $:any,swal:any;
 @Component({
   selector: 'app-devices-info',
@@ -16,7 +17,11 @@ export class DevicesInfoComponent implements OnInit {
   tilesLoader  = false;
   listLoader:HttpRequestLoader = new HttpRequestLoader();
   devicesInfo:Array<any> = new Array<any>();
-  constructor(public azugaService:AzugaService,public referenceService:ReferenceService,public authenticationService:AuthenticationService,public xtremandLogger:XtremandLogger) {
+  reAssignDataLoader = false;
+  selectedDeviceInfo:any;
+  customResponse:CustomResponse = new CustomResponse();
+  constructor(public azugaService:AzugaService,public referenceService:ReferenceService,public authenticationService:AuthenticationService,
+    public xtremandLogger:XtremandLogger,public properties:Properties) {
    }
 
   ngOnInit() {
@@ -38,4 +43,33 @@ export class DevicesInfoComponent implements OnInit {
     );
   }
 
+  openReAssignModalPopUp(deviceInfo:any){
+    this.selectedDeviceInfo = deviceInfo;
+    $('#reassign-data-modal-popup').modal('show');
+  }
+
+  hideReAssignDataModalPopUp(){
+    $('#reassign-data-modal-popup').modal('hide');
+    $('#fromCustomerId').val();
+    $('#toCustomerId').val();
+    this.selectedDeviceInfo = {};
+  }
+
+  transferData(){
+    this.customResponse = new CustomResponse();
+    this.reAssignDataLoader = true;
+    this.azugaService.moveDevicesData().subscribe(
+      response=>{
+        let statusCode = response.statusCode;
+        if(statusCode==200){
+          this.customResponse = new CustomResponse('SUCCESS',"Request to moving data is accepted and being is processed",true);
+        }else{
+          this.customResponse = new CustomResponse('ERROR',this.properties.serverErrorMessage,true);
+        }
+        this.reAssignDataLoader = false;
+      },error=>{
+        this.customResponse = new CustomResponse('ERROR',this.properties.serverErrorMessage,true);
+      }
+    );
+  }
 }
