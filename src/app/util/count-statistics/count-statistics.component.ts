e7f42b5a6fc4e3b16e18dc8a432056ed0a65e88a
csravan-stratapps <csravan@stratapps.com>
2022-11-21 17:32:07 +0530

    count-statistics.component
    XNFR-139

---
+++ src/app/util/count-statistics/count-statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-count-statistics',
+  templateUrl: './count-statistics.component.html',
+  styleUrls: ['./count-statistics.component.css']
+})
+export class CountStatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
