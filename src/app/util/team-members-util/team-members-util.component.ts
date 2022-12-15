dd7bb209cf425f04c7271f032f95126e87493eda
csravan-stratapps <csravan@stratapps.com>
2022-12-15 12:25:52 +0530

    SweetAlert message updated

--- src/app/util/team-members-util/team-members-util.component.ts
+++ src/app/util/team-members-util/team-members-util.component.ts
@@ -20,6 +20,7 @@ import { Properties } from '../../common/models/properties';
 import { RegularExpressions } from '../../common/models/regular-expressions';
 import { VanityLoginDto } from '../../util/models/vanity-login-dto';
 import { AnalyticsCountDto } from 'app/core/models/analytics-count-dto';
+import { SweetAlertParameterDto } from 'app/common/models/sweet-alert-parameter-dto';
 
 declare var $:any, swal: any;
 @Component({
@@ -82,6 +83,7 @@ export class TeamMembersUtilComponent implements OnInit, OnDestroy {
   analyticsCountDto:AnalyticsCountDto = new AnalyticsCountDto();
   showPrimaryAdminConfirmSweetAlert = false;
   selectedPrimaryAdminTeamMemberUserId = 0;
+  primaryAdminSweetAlertParameterDto:SweetAlertParameterDto = new SweetAlertParameterDto();
   constructor(public logger: XtremandLogger, public referenceService: ReferenceService, private teamMemberService: TeamMemberService,
     public authenticationService: AuthenticationService, private pagerService: PagerService, public pagination: Pagination,
     private fileUtil: FileUtil, public callActionSwitch: CallActionSwitch, public userService: UserService, private router: Router,
@@ -97,6 +99,7 @@ export class TeamMembersUtilComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.primaryAdminSweetAlertParameterDto.confirmButtonText = "Yes, Change It";
     this.isTeamMemberModule = this.moduleName == 'teamMember';
     this.moveToTop = "/home/team/add-team" == this.referenceService.getCurrentRouteUrl();
     this.findAll(this.pagination);
@@ -881,7 +884,7 @@ export class TeamMembersUtilComponent implements OnInit, OnDestroy {
   }
  /********XNFR-139*********/
   setPrimaryAdminOptions(teamMember:any){
-    if(teamMember.status=='APPROVE'){
+    if(teamMember.status=='APPROVE' && teamMember.secondAdmin && !this.isLoggedInAsTeamMember){
       this.showPrimaryAdminConfirmSweetAlert = true;
       this.selectedPrimaryAdminTeamMemberUserId = teamMember.teamMemberUserId;
     }
