da39996c015fff931e7235538c57ffe98d29e6b8
tharakaram <tharakaram@192.168.0.112>
2023-02-09 02:04:07 +0530

    remove footer in bottom when user disabled footer

--- src/env.js
+++ src/env.js
@@ -8,8 +8,10 @@
 	// Setting this to false will disable console output
 	/*************Local*******************************/
 	window.__env.CLIENT_URL = 'http://localhost:4200/'; // local client app server
-	window.__env.SERVER_URL = 'http://localhost:8090/'; // xamplify backend production server
-	window.__env.imagesHost = "http://127.0.0.1:8887/images/";
+	 window.__env.SERVER_URL = 'http://localhost:8090/'; // xamplify backend production server
+	 window.__env.imagesHost = "http://127.0.0.1:8887/images/";
+	// window.__env.SERVER_URL =  'https://aravindu.com/';
+	//  window.__env.imagesHost = "https://aravindu.com/vod/images/";
 
 	//USE BELOW URLS FOR CLIENT AND SERVER CONNECTIONS
 	/********************Production************************/
