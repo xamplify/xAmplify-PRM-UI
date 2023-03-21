21b8c8f10bc8b61887372b5ba9966591882c9181
csravan-stratapps <csravan@stratapps.com>
2023-03-21 18:23:49 +0530

    loader reverted
    XNFR-222

--- src/app/lefside-navigation-loader/lefside-navigation-loader.component.ts
+++ src/app/lefside-navigation-loader/lefside-navigation-loader.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit,Input } from '@angular/core';
-import { $ } from 'protractor';
 
 @Component({
   selector: 'app-lefside-navigation-loader',
@@ -9,20 +8,18 @@ import { $ } from 'protractor';
 export class LefsideNavigationLoaderComponent implements OnInit {
 	className = "col-xs-12";
 	items: number[] = [];
-	@Input() loadDefaultCss = false;
+	@Input() topNavbarLoader = false;
 	constructor() { }
 
 	ngOnInit() {
-		if(this.loadDefaultCss){
-			$(".timeline-item").css({'background': '#fff','border-color':'#e5e6e9 #dfe0e4 #d0d1d5'});
-			$(".animated-background").css({'background': 'linear-gradient(to right, #eeeeee 8%, #dddddd 18%, #eeeeee 33%)'});
+		if(this.topNavbarLoader){
 			this.items.push(1);
 		}else{
-			$(".animated-background").css({'background': 'linear-gradient(to right, #00000008 8%, #172d44 18%, #172d44  33%)'});
 			for(let i=0;i<=6;i++){
 				this.items.push(i);
 			}
 		}
+		
 	}
 
 }
