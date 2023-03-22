eb482491b8dd0295442f96b11604565c569a930f
csravan-stratapps <csravan@stratapps.com>
2023-03-23 00:12:23 +0530

    vanity url service updated
    XNFR-224

--- src/app/vanity-url/services/vanity.url.service.ts
+++ src/app/vanity-url/services/vanity.url.service.ts
@@ -93,8 +93,8 @@ export class VanityURLService {
 
 
   isVanityURLEnabled() {
-   //let url =window.location.hostname;
-    let url="stratappsvendor.xamplify.com";
+   let url =window.location.hostname;
+    //let url="stratappsvendor.xamplify.com";
       if (!url.includes("192.168")) {
       let domainName = url.split('.');
       if (domainName.length > 2) {
