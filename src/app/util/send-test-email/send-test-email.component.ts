3e0dda8d461bdb58b0f914ee430ebfce01591646
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2023-06-30 18:34:09 +0530

    Event Emitter Added XNFR-317

--- src/app/util/send-test-email/send-test-email.component.ts
+++ src/app/util/send-test-email/send-test-email.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter, ViewChild } from '@angular/core';
+import { CustomResponse } from '../../common/models/custom-response';
+import { Properties } from '../../common/models/properties';
+import { XtremandLogger } from '../../error-pages/xtremand-logger.service';
+import { AuthenticationService } from '../../core/services/authentication.service';
+import { ReferenceService } from 'app/core/services/reference.service';
+
+declare var $:any;
 
 @Component({
   selector: 'app-send-test-email',
   templateUrl: './send-test-email.component.html',
-  styleUrls: ['./send-test-email.component.css']
+  styleUrls: ['./send-test-email.component.css'],
+  providers: [Properties]
 })
 export class SendTestEmailComponent implements OnInit {
 
   @Input() id:number = 0;
   @Output() sendTestEmailComponentEventEmitter = new EventEmitter();
-
-  constructor() { }
+  subject = "";
+  emailIds = [];
+  modalPopupId = "send-test-email-modal-popup";
+  sent = false;
+  processing = false;
+  customResponse: CustomResponse = new CustomResponse();
+  success = true;
+  constructor(public referenceService:ReferenceService) { }
 
   ngOnInit() {
+    this.referenceService.openModalPopup(this.modalPopupId);
   }
 
   callEventEmitter(){
+    this.referenceService.closeModalPopup(this.modalPopupId);
     this.sendTestEmailComponentEventEmitter.emit();
   }
 
