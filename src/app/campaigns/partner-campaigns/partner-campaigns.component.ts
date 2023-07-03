d1d3f67fc6c5763a52547a9f1f3b13cd6ccb8a9e
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2023-07-03 14:16:53 +0530

    issue is fixed in filter option

--- src/app/campaigns/partner-campaigns/partner-campaigns.component.ts
+++ src/app/campaigns/partner-campaigns/partner-campaigns.component.ts
@@ -248,6 +248,8 @@ export class PartnerCampaignsComponent implements OnInit,OnDestroy {
                         }
                         this.modulesDisplayType.isFolderListView = false;
                         this.modulesDisplayType.isFolderGridView = false;
+                        this.campaignType = 'regular';
+                        this.pagination.campaignType = this.campaignType;
                         this.listCampaign(this.pagination);
                     }else if(this.modulesDisplayType.isFolderGridView){
                         this.setViewType('Folder-Grid');
