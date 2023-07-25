34ff800db777af03a26a00f0323a5c0fad45c7ba
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2023-07-25 15:22:44 +0530

    https://xamplify.atlassian.net/browse/XBI-1871

--- src/app/component-can-deactivate.ts
+++ src/app/component-can-deactivate.ts
@@ -16,20 +16,28 @@ export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate
   modalRef: any;
 
   constructor(private authenticationService: AuthenticationService,
-    private modalService: BsModalService) {};
+    private modalService: BsModalService) {
+      
+    };
 
   canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
-    this.authenticationService.module.contentLoader = false;
-    this.authenticationService.leftSideMenuLoader = false;
-    // if there are no pending changes, just allow deactivation; else confirm first
-    return component.canDeactivate() ?
+     this.authenticationService.module.contentLoader = false;
+      this.authenticationService.leftSideMenuLoader = false;
+    if(component!=null){
+      return component.canDeactivate() ?
       true :
       // NOTE: this warning message will only be shown when navigating elsewhere within your angular app;
       // when navigating away from your angular app, the browser will show a generic warning message
       // see http://stackoverflow.com/a/42207299/7307355
       //https://stackoverflow.com/questions/35922071/warn-user-of-unsaved-changes-before-leaving-page
-     confirm('You have unsaved changes. Press Cancel to save these changes, or OK to lose these changes.');
-    // this.openConfirmDialog();
+      this.callConfrimMethod();
+    }else{
+      this.callConfrimMethod();
+    }
+    }
+
+    callConfrimMethod(){
+     return confirm("You haven't saved your changes. Are you sure you want to leave this page?");
     }
 
     openConfirmDialog() {
