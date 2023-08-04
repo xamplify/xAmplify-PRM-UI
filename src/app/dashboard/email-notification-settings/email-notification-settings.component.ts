6ef0219d8e8b868cc33811ce5435c563a3dfbcbe
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2023-08-04 12:57:37 +0530

    email-notification-settings option added

---
+++ src/app/dashboard/email-notification-settings/email-notification-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-notification-settings',
+  templateUrl: './email-notification-settings.component.html',
+  styleUrls: ['./email-notification-settings.component.css']
+})
+export class EmailNotificationSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
