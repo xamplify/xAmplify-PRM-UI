87b2af1a67ff1f12bdbc9f43496b3e40925c74af
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2023-09-02 12:13:17 +0530

    email-template-list-and-grid component added

---
+++ src/app/util/email-templates/email-templates-list-and-grid-view/email-templates-list-and-grid-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-templates-list-and-grid-view',
+  templateUrl: './email-templates-list-and-grid-view.component.html',
+  styleUrls: ['./email-templates-list-and-grid-view.component.css']
+})
+export class EmailTemplatesListAndGridViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
