87185b7c69ba37f9ca9aa19c89fa163e5a6c106d
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2023-09-20 14:36:28 +0530

    Css updated for select-email-template
    XNFR-368

--- src/app/util/select-email-template/select-email-template.component.ts
+++ src/app/util/select-email-template/select-email-template.component.ts
@@ -6,9 +6,10 @@ import { SortOption } from 'app/core/models/sort-option';
 import { AuthenticationService } from 'app/core/services/authentication.service';
 import { PagerService } from 'app/core/services/pager.service';
 import { UtilService } from 'app/core/services/util.service';
+import { EmailTemplate } from 'app/email-template/models/email-template';
 import { EmailTemplateService } from 'app/email-template/services/email-template.service';
 import { XtremandLogger } from 'app/error-pages/xtremand-logger.service';
-
+declare var $:any;
 @Component({
   selector: 'app-select-email-template',
   templateUrl: './select-email-template.component.html',
@@ -31,6 +32,15 @@ export class SelectEmailTemplateComponent implements OnInit {
   selectedEmailTemplateNameForPreview = "";
   isPreviewEmailTemplateButtonClicked = false;
   emailTemplatesSortOption:SortOption = new SortOption();
+  isShowEditTemplateMessageDiv: boolean;
+  isEditTemplateLoader: boolean;
+  referenceService: any;
+  beeContainerInput: any;
+  isShowEditTemplatePopup: boolean;
+  editTemplateMergeTagsInput: {};
+  loggedInUserId: any;
+  templateMessageClass: string;
+  templateUpdateMessage: string;
   constructor(private campaignService:CampaignService,private xtremandLogger:XtremandLogger,
     private pagerService:PagerService,private properties:Properties,
     private authenticationService:AuthenticationService,
@@ -39,7 +49,8 @@ export class SelectEmailTemplateComponent implements OnInit {
   ngOnInit() {
     this.emailTemplatesLoader = true;
     this.emailTemplatesPagination.maxResults = 4;
-    this.emailTemplatesPagination.userId = this.authenticationService.getUserId();
+    this.loggedInUserId = this.authenticationService.getUserId();
+    this.emailTemplatesPagination.userId = this.loggedInUserId;
     this.findEmailTemplates(this.emailTemplatesPagination);
   }
 
@@ -112,6 +123,104 @@ previewEmailTemplate(emailTemplate:any){
     this.selectedEmailTemplateNameForPreview = emailTemplate.name;
     this.isPreviewEmailTemplateButtonClicked = true;
 
+}
+
+previewEmailTemplateModalPopupEventReceiver(){
+  this.selectedEmailTemplateIdForPreview = 0;
+  this.selectedEmailTemplateNameForPreview = "";
+  this.isPreviewEmailTemplateButtonClicked = false;
+}
+
+editTemplate(emailTemplate:any){
+  this.isShowEditTemplateMessageDiv = false;
+  if (emailTemplate['emailTemplateType'] != 'UPLOADED' && emailTemplate.userDefined) {
+      this.isEditTemplateLoader = true;
+      this.referenceService.goToTop();
+     $('#campaign-details-and-launch-tabs').hide(600);
+     $('#edit-campaign-template').show(600);
+     this.beeContainerInput['emailTemplateName'] = emailTemplate.name;
+     this.emailTemplateService.findJsonBody(emailTemplate.id).subscribe(
+          response => {
+              this.beeContainerInput['module'] = "emailTemplates";
+              this.beeContainerInput['jsonBody'] = response;
+              this.beeContainerInput['id'] = emailTemplate.id;
+              /****XBI-1685******/
+              let anyCoBrandingTemplate = emailTemplate['regularCoBrandingTemplate'] || emailTemplate['surveyCoBrandingTemplate'] ||
+              emailTemplate['videoCoBrandingTemplate'] || emailTemplate['beeEventCoBrandingTemplate'] ;
+              this.beeContainerInput['anyCoBrandingTemplate'] =anyCoBrandingTemplate;
+              this.beeContainerInput['isVideoTemplate'] = emailTemplate['videoTemplate'] || emailTemplate['videoCoBrandingTemplate'] || emailTemplate['beeVideoTemplate'];
+              /****XBI-1685******/
+              this.isShowEditTemplatePopup = true;
+              this.isEditTemplateLoader = false;
+          }, error => {
+              this.hideEditTemplateDiv();
+              this.referenceService.showSweetAlertServerErrorMessage();
+          }
+      );
+  } else {
+      this.referenceService.showSweetAlertErrorMessage('Uploaded Templates Cannot Be Edited');
+  }
+}
+
+hideEditTemplateDiv() {
+  $('#edit-campaign-template').hide(600);
+  this.isShowEditTemplatePopup = false;
+  this.isEditTemplateLoader = false;
+  this.beeContainerInput = {};
+  this.editTemplateMergeTagsInput = {};
+  $('#campaign-details-and-launch-tabs').show(600);
+}
+
+updateTemplate(event:any){
+  this.ngxLoading =true;
+  let module = event['module'];
+  this.updateEmailTemplate(event);
+}
+updateEmailTemplate(event: any) {
+ let emailTemplate = new EmailTemplate();
+ emailTemplate.id = event.id;
+ emailTemplate.jsonBody = event.jsonContent;
+ emailTemplate.body = event.htmlContent;
+ emailTemplate.userId = this.loggedInUserId;
+ this.emailTemplateService.updateJsonAndHtmlBody(emailTemplate).subscribe(
+     response => {
+         if (response.statusCode == 200) {
+             this.showTemplateUpdatedSuccessMessage();
+         } else if (response.statusCode == 500) {
+             this.showUpdateTemplateErrorMessage(response.message);
+         }                
+     }, error => {
+         this.showTemplateUpdateErrorMessage();
+     }
+ )
+}
+
+
+showTemplateUpdatedSuccessMessage(){
+  this.ngxLoading =false;
+  this.isShowEditTemplateMessageDiv = true;
+  this.templateMessageClass = "alert alert-success";
+  this.templateUpdateMessage = "Template Updated Successfully";
+  this.referenceService.goToTop();
+}
+
+showTemplateUpdateErrorMessage(){
+  this.ngxLoading =false;
+  this.templateMessageClass = "alert alert-danger";
+  this.templateUpdateMessage = this.properties.serverErrorMessage;
+  this.isShowEditTemplateMessageDiv = true;
+}
+
+showUpdateTemplateErrorMessage(message: string){
+  this.ngxLoading =false;
+  this.templateMessageClass = "alert alert-danger";
+  if (message != undefined && message != null && message.trim().length > 0) {
+      this.templateUpdateMessage = message;
+  } else {
+      this.templateUpdateMessage = this.properties.serverErrorMessage;
+  }
+  
+  this.isShowEditTemplateMessageDiv = true;
 }
   
 
