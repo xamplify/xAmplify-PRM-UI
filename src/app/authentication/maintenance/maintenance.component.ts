5e33b536c14cae86fe4a4613ddf4d245884468d2
cvenkat <cvenkat@cvenkat-sa.stratapps.com>
2023-10-10 11:05:44 +0530

    xamplify maintenance website

--- src/app/authentication/maintenance/maintenance.component.ts
+++ src/app/authentication/maintenance/maintenance.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-maintenance',
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintenanceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
-  ngOnInit() {
+  ngOnInit(): void {}
+
+  // number countdown start
+  ngAfterViewInit(): void {
+    this.animateCount();
+    this.animateCount1();
+  }
+  animateCount() {
+    const element = this.el.nativeElement.querySelector('.count');
+    const finalValue = parseInt(element.innerText, 10);
+    let currentValue = 0;
+
+    const animationInterval = setInterval(() => {
+      if (currentValue < finalValue) {
+        currentValue++;
+        element.innerText = currentValue.toString();
+      } else {
+        clearInterval(animationInterval);
+      }
+    }, 40);
   }
 
+  animateCount1() {
+    const element = this.el.nativeElement.querySelector('.count1');
+    const finalValue = parseInt(element.innerText, 10);
+    let currentValue = 0;
+
+    const animationInterval = setInterval(() => {
+      if (currentValue < finalValue) {
+        currentValue++;
+        element.innerText = currentValue.toString();
+      } else {
+        clearInterval(animationInterval);
+      }
+    }, 40); // Adjust the interval for the desired animation speed
+  }
+   // number countdown end
+
+  // header navbar start
+  isNavbarBackgroundVisible: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const scrollY = window.scrollY;
+    this.isNavbarBackgroundVisible = scrollY > 50;
+  }
+    // header navbar end
 }
