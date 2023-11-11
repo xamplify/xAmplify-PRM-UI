7e560e5609169f4332453ee3e0f7b91374a37261
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2023-11-11 19:06:31 +0530

    Finding campaigs api added
    XNFR-342

--- src/app/common/share-campaigns/share-campaigns.component.ts
+++ src/app/common/share-campaigns/share-campaigns.component.ts
@@ -1,15 +1,133 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Pagination } from 'app/core/models/pagination';
+import { AuthenticationService } from 'app/core/services/authentication.service';
+import { PagerService } from 'app/core/services/pager.service';
+import { ReferenceService } from 'app/core/services/reference.service';
+import { Properties } from '../models/properties';
+import { VanityURLService } from 'app/vanity-url/services/vanity.url.service';
+import { HttpRequestLoader } from 'app/core/models/http-request-loader';
+import { SortOption } from 'app/core/models/sort-option';
+import { CustomResponse } from '../models/custom-response';
+import { XtremandLogger } from 'app/error-pages/xtremand-logger.service';
+import { CampaignService } from 'app/campaigns/services/campaign.service';
+import { UtilService } from 'app/core/services/util.service';
+
+
+
+declare var $:any;
 
 @Component({
   selector: 'app-share-campaigns',
   templateUrl: './share-campaigns.component.html',
-  styleUrls: ['./share-campaigns.component.css']
+  styleUrls: ['./share-campaigns.component.css'],
+  providers: [Pagination, HttpRequestLoader, Properties, SortOption]
 })
 export class ShareCampaignsComponent implements OnInit {
 
-  constructor() { }
+  @Output() shareCampaignsEventEmitter = new EventEmitter();
+  pagination:Pagination = new Pagination();
+  firstName = "";
+  lastName = "";
+  companyName = "";
+  type = "";
+  newEmailIdsAreAdded = false;
+  customResponse:CustomResponse = new CustomResponse();
+  httpRequestLoader:HttpRequestLoader = new HttpRequestLoader();
+  sortOption:SortOption = new SortOption();
+  selectedCampaignIds = [];
+  isHeaderCheckBoxChecked = false;
+
+  constructor(public authenticationService:AuthenticationService,private referenceService:ReferenceService,
+    private pagerService:PagerService,private vanityUrlService:VanityURLService,private xtremandLogger:XtremandLogger,
+    private campaignService:CampaignService,public utilService:UtilService) { }
 
   ngOnInit() {
+    
   }
+
+  loadCampaigns(selectedPartnerListId:number,contact:any,type:string){
+    this.pagination.partnerOrContactEmailId = contact.emailId;
+    this.pagination.partnerId = contact.id;
+    this.firstName = contact.firstName;
+    this.lastName = contact.lastName;
+    this.companyName = contact.contactCompany;
+    this.pagination.userListId = selectedPartnerListId;
+    this.type = type;
+    this.newEmailIdsAreAdded = false;
+    if(type=="Contact" && this.vanityUrlService.isVanityURLEnabled()){
+      this.pagination.vendorCompanyProfileName = this.authenticationService.companyProfileName;
+      this.pagination.vanityUrlFilter = true;
+    }
+    this.findCampaigns(this.pagination);
+
+  }
+
+
+  findCampaigns(pagination: Pagination) {
+    this.customResponse = new CustomResponse();
+    this.referenceService.startLoader(this.httpRequestLoader);
+    this.campaignService.listCampaignsByUserListIdAndUserId(pagination, this.type)
+        .subscribe(
+        response => {
+            const data = response.data;
+            pagination.totalRecords = data.totalRecords;
+            this.sortOption.totalRecords = data.totalRecords;
+            let campaigns = data.campaigns;
+            $.each(campaigns, function(_index: number, campaign: any) {
+                campaign.displayTime = new Date(campaign.launchTimeInString);
+            });
+            pagination = this.pagerService.getPagedItems(pagination, campaigns);
+            /*******Header checkbox will be chcked when navigating through page numbers*****/
+            var campaignIds = this.pagination.pagedItems.map(function(a) { return a.id; });
+            var items = $.grep(this.selectedCampaignIds, function(element: any) {
+                return $.inArray(element, campaignIds) !== -1;
+            });
+            if (items.length == campaignIds.length) {
+                this.isHeaderCheckBoxChecked = true;
+            } else {
+                this.isHeaderCheckBoxChecked = false;
+            }
+            this.referenceService.stopLoader(this.httpRequestLoader);
+        },
+        (_error: any) => {
+            this.customResponse = this.referenceService.showServerErrorResponse(this.httpRequestLoader);
+        });
+}
+
+/********************Pagaination&Search Code*****************/
+
+  /*************************Sort********************** */
+  sortBy(text: any) {
+    this.sortOption.selectedShareCampaignDropDownOption = text;
+    this.getAllFilteredResults(this.pagination);
+  }
+
+
+  /*************************Search********************** */
+  searchCampaigns() {
+    this.getAllFilteredResults(this.pagination);
+  }
+
+  paginationDropdown(items: any) {
+    this.getAllFilteredResults(this.pagination);
+  }
+
+  /************Page************** */
+  setPage(event: any) {
+    this.customResponse = new CustomResponse();
+    this.pagination.pageIndex = event.page;
+    this.findCampaigns(this.pagination);
+  }
+  
+
+  getAllFilteredResults(pagination: Pagination) {
+    this.customResponse = new CustomResponse();
+    pagination.pageIndex = 1;
+    pagination.searchKey = this.sortOption.searchKey;
+    pagination = this.utilService.sortOptionValues(this.sortOption.selectedShareCampaignDropDownOption, pagination);
+    this.findCampaigns(pagination);
+  }
+  findCampaignsOnKeyPress(keyCode: any) { if (keyCode === 13) { this.searchCampaigns(); } }
+
 
 }
--- src/app/common/share-unpublished-content/share-unpublished-content.component.ts
+++ src/app/common/share-unpublished-content/share-unpublished-content.component.ts
@@ -1,26 +1,33 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,ViewChild } from '@angular/core';
 import { AuthenticationService } from 'app/core/services/authentication.service';
 import { ReferenceService } from 'app/core/services/reference.service';
+import { Properties } from '../models/properties';
+import { ShareCampaignsComponent } from '../share-campaigns/share-campaigns.component';
 
 @Component({
   selector: 'app-share-unpublished-content',
   templateUrl: './share-unpublished-content.component.html',
-  styleUrls: ['./share-unpublished-content.component.css']
+  styleUrls: ['./share-unpublished-content.component.css'],
+  providers:[Properties]
 })
 export class ShareUnpublishedContentComponent implements OnInit {
 
-    /**XNFR-342****/
-    selectedIndex = 0;
-    headerText = "";
-    @Input() hasCampaignAccess = false;
-    @Input() hasDamAccess = false;
-    @Input() hasLmsAccess =false;
-    @Input() hasPlaybookAccess = false;
-    showFilterOptions = false;
-    modalPopUpId = "shareUnPublishedContentPopUp";
-    modalHeaderText = "";
-    filterOptions:Array<string> = new Array<string>();
-  constructor(public authenticationService:AuthenticationService,public referenceService:ReferenceService) { }
+  /**XNFR-342****/
+  selectedIndex = 0;
+  headerText = "";
+  @Input() hasCampaignAccess = false;
+  @Input() hasDamAccess = false;
+  @Input() hasLmsAccess =false;
+  @Input() hasPlaybookAccess = false;
+  showFilterOptions = false;
+  modalPopUpId = "shareUnPublishedContentPopUp";
+  modalHeaderText = "";
+  filterOptions:Array<string> = new Array<string>();
+  @ViewChild('shareCampaignsComponent') shareCampaignsComponent: ShareCampaignsComponent;
+  selectedPartnerListId = 0;
+  contact:any;
+  type  = "";
+  constructor(public authenticationService:AuthenticationService,public referenceService:ReferenceService,public properties:Properties) { }
 
   ngOnInit() {
   }
@@ -37,22 +44,25 @@ export class ShareUnpublishedContentComponent implements OnInit {
     this.addFilterOptions();
     let firstFilterOption = this.filterOptions[0];
     this.modalHeaderText = "Please Select "+firstFilterOption;
+    this.contact = contact;
+    this.type = type;
+    this.selectedPartnerListId = partnerListId;
     this.referenceService.openModalPopup(this.modalPopUpId);
 
   }
 
   private addFilterOptions() {
     if(this.hasCampaignAccess) {
-      this.filterOptions.push("Campaigns");
+      this.filterOptions.push(this.properties.campaignsHeaderText);
     }
     if(this.hasDamAccess) {
-      this.filterOptions.push("Dam");
+      this.filterOptions.push(this.properties.assetsHeaderText);
     }
     if(this.hasLmsAccess) {
-      this.filterOptions.push("Tracks");
+      this.filterOptions.push(this.properties.tracksHeaderText);
     }
     if(this.hasPlaybookAccess) {
-      this.filterOptions.push("Playbooks");
+      this.filterOptions.push(this.properties.playBooksHeaderText);
     }
   }
 
@@ -60,10 +70,28 @@ export class ShareUnpublishedContentComponent implements OnInit {
     this.filterOptions = [];
     this.modalHeaderText = "";
     this.showFilterOptions = false;
+    this.selectedPartnerListId = 0;
+    this.contact = {};
+    this.type = "";
   }
 
   closePopup(){
     this.referenceService.closeModalPopup(this.modalPopUpId);
+  }
+
+  applyFilter(index:number,filterOption:string){
+    this.selectedIndex = index;
+    this.modalHeaderText = "Please Select "+filterOption;
+    if(filterOption==this.properties.campaignsHeaderText){
+      this.shareCampaignsComponent.loadCampaigns(this.selectedPartnerListId,this.contact,this.type);
+    }else if(filterOption==this.properties.assetsHeaderText){
+    }else if(filterOption==this.properties.tracksHeaderText){
+    }else if(filterOption==this.properties.playBooksHeaderText){
+    }
+    
+  }
+
+  shareCampaignsEventReceiver(event:any){
   }
 
 }
