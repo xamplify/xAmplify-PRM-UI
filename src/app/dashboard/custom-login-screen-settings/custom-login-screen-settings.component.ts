a45b5e4ebd1170a45d896a3d4e2bb798da77c214
ctharakaram <ctharakaram@ctharakaram-sa.stratapps.com>
2023-12-06 10:56:06 +0530

    domain name is missing issue is resolved

--- src/app/dashboard/custom-login-screen-settings/custom-login-screen-settings.component.ts
+++ src/app/dashboard/custom-login-screen-settings/custom-login-screen-settings.component.ts
@@ -612,7 +612,7 @@ export class CustomLoginScreenSettingsComponent implements OnInit {
 
   }
   openNewTab(){
-    const newTabUrl = this.authenticationService.APP_URL +'login/preview'; // Replace with the URL you want to open
+    const newTabUrl = this.authenticationService.DOMAIN_URL +'login/preview'; // Replace with the URL you want to open
     // Open the URL in a new tab
     window.open(newTabUrl, '_blank');
   }
