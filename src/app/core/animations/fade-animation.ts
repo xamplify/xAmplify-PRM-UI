9eb688fcabe732445341bb0546594735f24cec68
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2023-12-15 18:00:07 +0530

    properties updated

--- src/app/core/animations/fade-animation.ts
+++ src/app/core/animations/fade-animation.ts
@@ -7,19 +7,8 @@ import {
 
 export const FadeAnimation = [
     
-   // the fade-in/fade-out animation.
-   trigger('simpleFadeAnimation', [
-
-    // the "in" style determines the "resting" state of the element when it is visible.
-
-    // fade in when created. this could also be written as transition('void => *')
-    transition(':enter', [
-      style({opacity: 0}),
-      animate(500)
-    ]),
-
-    // fade out when destroyed. this could also be written as transition('void => *')
-    transition(':leave',
-      animate(500, style({opacity: 0})))
+  trigger('slowAnimate', [
+    transition(':enter', [style({opacity: '0'}), animate(500)]),
+    transition(':leave', [style({opacity: '1'}), animate(500, style({opacity: '0'}))]),
   ])
 ];
