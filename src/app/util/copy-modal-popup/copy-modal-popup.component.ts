db5e1c894bcef61eec28b43d617542db87f5dcb5
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2024-01-05 16:51:59 +0530

    CopyModalPopupComponent added

---
+++ src/app/util/copy-modal-popup/copy-modal-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-copy-modal-popup',
+  templateUrl: './copy-modal-popup.component.html',
+  styleUrls: ['./copy-modal-popup.component.css']
+})
+export class CopyModalPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
