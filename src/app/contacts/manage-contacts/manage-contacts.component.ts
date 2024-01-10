7f6abdcfd6c52ee32ebc3b0bd7cd5684303e96da
pvamsilakshman999 <pvamsi@stratapps.com>
2024-01-10 14:29:36 +0530

    XBI-963 RESOLVED

--- src/app/contacts/manage-contacts/manage-contacts.component.ts
+++ src/app/contacts/manage-contacts/manage-contacts.component.ts
@@ -2179,9 +2179,15 @@ export class ManageContactsComponent implements OnInit, AfterViewInit, AfterView
 			this.contactService.mailSend(this.pagination)
 				.subscribe(
 					data => {
-						this.emailNotificationCustomResponse = new CustomResponse('SUCCESS', this.properties.EMAIL_SENT_SUCCESS, true);
-						this.contactService.successMessage = true;
-						//this.listContactsByType(this.contactsByType.selectedCategory);
+						if(data.statusCode==200){
+							this.emailNotificationCustomResponse = new CustomResponse('SUCCESS', this.properties.EMAIL_SENT_SUCCESS, true);
+							this.contactService.successMessage = true;
+						}
+						else{
+							this.customResponse = new CustomResponse('ERROR', data.message, true);
+
+						}
+					
 					},
 					(error: any) => {
 						this.customResponse = new CustomResponse('ERROR', 'Some thing went wrong please try after some time.', true);
