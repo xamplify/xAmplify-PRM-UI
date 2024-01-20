8335204d585f23ac8b2734f24c1b52f0b54ec381
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2024-01-20 18:11:22 +0530

    connectoWebSocket Condition added

--- src/app/core/topnavbar/topnavbar.component.ts
+++ src/app/core/topnavbar/topnavbar.component.ts
@@ -75,7 +75,9 @@ export class TopnavbarComponent implements OnInit,OnDestroy {
     public refService: ReferenceService, public logger: XtremandLogger,public properties: Properties,private translateService: TranslateService,
     private vanityServiceURL:VanityURLService) {
     try{
-      this.connectToWebSocket();
+      if(this.authenticationService.isLocalHost() || this.authenticationService.isQADomain()){
+        this.connectToWebSocket();
+      }
     this.isLoggedInFromAdminSection = this.utilService.isLoggedInFromAdminPortal();
     this.isLoggedInAsPartner = this.utilService.isLoggedAsPartner();
     this.isLoggedInAsTeamMember = this.utilService.isLoggedAsTeamMember();
