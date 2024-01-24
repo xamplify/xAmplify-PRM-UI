e1f814db228e84be7023113279df05621b3f0378
ctharakaram <ctharakaram@ctharakaram-sa.stratapps.com>
2024-01-24 19:25:48 +0530

    added gradiant colors to neumorphsim dark theme buttons

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -405,7 +405,7 @@ export class HomeComponent implements OnInit {
             && activeThemeDto.name === "Neumorphism Dark(Beta)" && !this.router.url.includes('home/help')) {
             this.authenticationService.isDarkForCharts = true;
             require("style-loader!../../../assets/admin/layout2/css/themes/neomorphism-dark.css");
-            // require("style-loader!../../../assets/admin/layout2/css/themes/neumorphism-light-dark-buttons.css")
+            require("style-loader!../../../assets/admin/layout2/css/themes/neumorphism-light-dark-buttons.css")
           }
           else if (activeThemeDto.defaultTheme && activeThemeDto.companyId === 1
             && activeThemeDto.name === "Neumorphism Light" && !this.router.url.includes('home/help')) {
