386af96fc21ba8838730aa5cc59db2e987721fa7
csravan-stratapps <43905540+csravan-stratapps@users.noreply.github.com>
2024-02-01 11:36:02 +0530

    "Opt-in" renamed as "Valid"

--- src/app/contacts/manage-contacts/manage-contacts.component.ts
+++ src/app/contacts/manage-contacts/manage-contacts.component.ts
@@ -2794,7 +2794,7 @@ resubscribeUserResult(event : any){
 			showCancelButton: true,
 			confirmButtonColor: '#54a7e9',
 			cancelButtonColor: '#999',
-			confirmButtonText: 'Yes, Mark as Opt-in!'
+			confirmButtonText: 'Yes, Mark as valid!'
 
 		}).then(function(myData: any) {
 			self.validateExcludedDetails(contactId,emailId );
