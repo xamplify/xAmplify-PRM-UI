3736e61a90db776c1d96c493aa8131cc2f8853de
mvishnushankar <mvishnushankar@mvishnushankar-sa.stratapps.com>
2024-03-19 15:40:37 +0530

    restricted halopsa crm tile for production domain.

--- src/app/dashboard/halopsa-authentication/halopsa-authentication.component.ts
+++ src/app/dashboard/halopsa-authentication/halopsa-authentication.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ReferenceService } from 'app/core/services/reference.service';
 
 @Component({
   selector: 'app-halopsa-authentication',
@@ -8,9 +9,10 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 export class HalopsaAuthenticationComponent implements OnInit {
   @Input() loggedInUserId: any;
   @Output() closeEvent = new EventEmitter<any>();
-  constructor() { }
+  constructor(public referenceService: ReferenceService,) { }
 
   ngOnInit() {
+    this.referenceService.goToTop();
   }
   closeForm() {
     console.log("Closed halopsa Auth")
