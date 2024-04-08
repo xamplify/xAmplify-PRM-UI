9183fd9f744c141d7a87a83be288dc1ce2c5afb0
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-04-08 12:22:43 +0530

    Processing UserLists Component Added

---
+++ src/app/dashboard/processing-user-lists/processing-user-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-processing-user-lists',
+  templateUrl: './processing-user-lists.component.html',
+  styleUrls: ['./processing-user-lists.component.css','../admin-report/admin-report.component.css']
+})
+export class ProcessingUserListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
