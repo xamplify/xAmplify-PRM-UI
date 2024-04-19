51037cb73c4e09ad2b15ac7ace9c9832f5e2a02b
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-04-19 11:45:41 +0530

    preview-asset-pdf component added

---
+++ src/app/common/preview-asset-pdf/preview-asset-pdf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-asset-pdf',
+  templateUrl: './preview-asset-pdf.component.html',
+  styleUrls: ['./preview-asset-pdf.component.css']
+})
+export class PreviewAssetPdfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
