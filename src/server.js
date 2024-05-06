9dbcadddc9b46159c73db37dc613cc2509c3d269
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-05-06 23:12:52 +0530

    server.js added

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+console.log(process.env.PORT);
+const port = process.env.PORT || 4200;
+
+app.use(express.static(path.join(__dirname, '../xtremandApp')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../xtremandApp/index.html'));
+});
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+let numberOfOnlineUsers = 0;
+
+io.on('connection', (socket) => {
+    console.log(socket);
+    numberOfOnlineUsers++;
+    io.emit('numberOfOnlineUsers', numberOfOnlineUsers);
+
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        numberOfOnlineUsers--;
+        io.emit('numberOfOnlineUsers', numberOfOnlineUsers);
+        console.log('User disconnected');
+    });
+});
+
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
