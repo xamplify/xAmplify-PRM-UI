bf4a6835e97cf62eb4b4c6564137fdb703ac25a8
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-05-20 00:05:14 +0530

    env.js updated

--- src/env.js
+++ src/env.js
@@ -7,47 +7,34 @@
 	// Whether or not to enable debug mode
 	// Setting this to false will disable the console output
 	/*************Local*******************************/
-
-
-
-
 	window.__env.CLIENT_URL = 'http://localhost:4200/'; // local client app server
 	window.__env.SERVER_URL = 'http://localhost:8090/'; // xamplify backend production server
 	window.__env.imagesHost = "http://localhost:8000/images/";
 	window.__env.domainName = "";
 	window.__evnv.PREVIEW_HOST = "http://localhost:5000/";
-
-
-
-
 	
 	//USE BELOW URLS FOR CLIENT AND SERVER CONNECTIONS
 	/********************Production************************/
-	// window.__env.CLIENT_URL = 'https://xamplify.io/'; // xamplify client app server
-	// window.__env.SERVER_URL = 'https://xamp.io/'; // xamplify backend production server
-	// window.__env.imagesHost = "https://xamp.io/vod/images/";
+	//window.__env.CLIENT_URL = 'https://xamplify.io/'; // xamplify client app server
+	//window.__env.SERVER_URL = 'https://xamp.io/'; // xamplify backend production server
+	//window.__env.imagesHost = "https://xamp.io/vod/images/";
 	//window.__evnv.PREVIEW_HOST = "http://localhost:5000/";
 
 	/*********************QA**********************/
-
-	// window.__env.CLIENT_URL = 'https://xamplify.co/';
-//	 window.__env.SERVER_URL =  'https://aravindu.com/';
-//	 window.__env.imagesHost = "https://aravindu.com/vod/images/";
-	// window.__evnv.PREVIEW_HOST = "http://localhost:5000/";
-
-
-
-
+	//window.__env.CLIENT_URL = 'https://xamplify.co/';
+	//window.__env.SERVER_URL =  'https://aravindu.com/';
+	//window.__env.imagesHost = "https://aravindu.com/vod/images/";
+	//window.__evnv.PREVIEW_HOST = "http://localhost:5000/";
 
 	/*********************Release**********************/
-	// window.__env.CLIENT_URL = 'https://xtremand.com/';
-	// window.__env.SERVER_URL =  'https://release.xamp.io/';
-	//  window.__env.imagesHost = "https://release.xamp.io/vod/images/";
+	//window.__env.CLIENT_URL = 'https://xtremand.com/';
+	//window.__env.SERVER_URL =  'https://release.xamp.io/';
+	//window.__env.imagesHost = "https://release.xamp.io/vod/images/";
 
 	/**********UI******************/
-	// window.__env.CLIENT_URL = 'https://x-amplify.com/';
-	// window.__env.SERVER_URL =  'https://aravindu.com/';
-	//  window.__env.imagesHost = "https://aravindu.com/vod/images/";
+	//window.__env.CLIENT_URL = 'https://x-amplify.com/';
+	//window.__env.SERVER_URL =  'https://aravindu.com/';
+	//window.__env.imagesHost = "https://aravindu.com/vod/images/";
 
 	window.__env.clientId = "6639d69f-523f-44ca-b809-a00daa26b367";
 	window.__env.clientSecret = "XnD77klwAeUFvYS66CbHMd107DMS441Etg9cCOVc63LTYko8NHa";
