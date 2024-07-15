0e31e9e709714917c9de1cf496a8852b8418012a
mvishnushankar <mvishnushankar@stratapps.com>
2024-07-15 22:55:11 +0530

    crm-settings component created

---
+++ src/app/dashboard/crm-settings/crm-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crm-settings',
+  templateUrl: './crm-settings.component.html',
+  styleUrls: ['./crm-settings.component.css']
+})
+export class CrmSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
