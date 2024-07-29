a10ab7401443758b8ba709dbdf255bf8b381f1fa
esantosh <esantosh@esantosh-sa.stratapps.com>
2024-07-29 11:48:11 +0530

    market place category dto code changes

--- src/app/dashboard/models/market-place-category.ts
+++ src/app/dashboard/models/market-place-category.ts
@@ -1,14 +1,10 @@
 export class MarketPlaceCategory {
     id: number;
-    tagNames:string[] = [];
     name: string;
     createdBy: string;
     updatedBy: number;
-    tagIds:number[] = [];
     displayTime: Date;
-    isTagNameValid= false;
     loggedInUserId: number;
     isValid= true;
     description : string;
-
 }
