883f91eb109eba22ec1a73d61632ec10b6a6f64c
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-08-19 15:15:51 +0530

    alert removed

--- src/app/campaigns/user-level-timeline/user-level-timeline.component.ts
+++ src/app/campaigns/user-level-timeline/user-level-timeline.component.ts
@@ -154,7 +154,6 @@ export class UserLevelTimelineComponent implements OnInit {
   goBack(){
     this.loading = true;
     let encodedUserId = this.referenceService.encodePathVariable(this.selectedUserId);
-    alert("Exiting Campaign Id "+this.analyticsCampaignId);
     let encodedCampaignId = this.referenceService.encodePathVariable(this.analyticsCampaignId);
     let url = "/home/campaigns/user-campaigns/"+this.previousRouterAlias+"/"+encodedUserId;
     if(this.navigatedFrom!=undefined && encodedCampaignId==undefined){
