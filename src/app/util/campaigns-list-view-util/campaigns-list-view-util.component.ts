d5fdf656dfcdb9d6c40311d76c9a750b50569579
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-08-23 10:41:49 +0530

    Encoded URL added to folder list view in campaign
    XNFR-648

--- src/app/util/campaigns-list-view-util/campaigns-list-view-util.component.ts
+++ src/app/util/campaigns-list-view-util/campaigns-list-view-util.component.ts
@@ -708,7 +708,10 @@ export class CampaignsListViewUtilComponent implements OnInit, OnDestroy {
                             if (campaign.hasAccess) {
                                 campaign.showGearIconOptions = data.data.showGearIconOptions;
                                 this.refService.campaignType = campaign.campaignType;
-                                this.router.navigate(['/home/campaigns/' + campaign.campaignId + '/details']);
+                                let campaignRouterParams = {};
+                                campaignRouterParams['campaignId'] = campaign.campaignId;
+                                campaignRouterParams['campaignTitle'] = campaign.campaignTitle;
+                                this.refService.goToCampaignAnalytics(campaignRouterParams);
                             } else {
                                 this.customResponse = new CustomResponse('ERROR', "You don't have access for this campaign", true);
                             }
