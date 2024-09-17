ff31fbac209564e673ceec7c41772e29a0130c0b
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-09-17 16:28:04 +0530

    findPaginatedCustomFields() Added
    XNFR-679

--- src/app/dashboard/user-profile/custom-fields/custom-fields.component.ts
+++ src/app/dashboard/user-profile/custom-fields/custom-fields.component.ts
@@ -1,22 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { Properties } from 'app/common/models/properties';
 import { HttpRequestLoader } from 'app/core/models/http-request-loader';
+import { Pagination } from 'app/core/models/pagination';
 import { SortOption } from 'app/core/models/sort-option';
 import { AuthenticationService } from 'app/core/services/authentication.service';
+import { PagerService } from 'app/core/services/pager.service';
 import { ReferenceService } from 'app/core/services/reference.service';
-
-
+import { CustomFieldService } from '../services/custom-field.service';
+import { CustomResponse } from 'app/common/models/custom-response';
 @Component({
   selector: 'app-custom-fields',
   templateUrl: './custom-fields.component.html',
   styleUrls: ['./custom-fields.component.css'],
-  providers:[SortOption,HttpRequestLoader]
+  providers:[SortOption,HttpRequestLoader,Properties]
 })
 export class CustomFieldsComponent implements OnInit {
-
-  constructor(public authenticationService:AuthenticationService,public referenceService:ReferenceService,public sortOption:SortOption) { }
+  httpRequestLoader:HttpRequestLoader = new HttpRequestLoader();
+  customResponse: CustomResponse = new CustomResponse();
+  customFields:Array<any> = new Array<any>();
+  pagination:Pagination = new Pagination();
+  constructor(public authenticationService:AuthenticationService,public referenceService:ReferenceService,public sortOption:SortOption,
+    public pagerService:PagerService,public properties:Properties,public customFieldService:CustomFieldService) { }
 
   ngOnInit() {
+    this.findPaginatedCustomFields(this.pagination);
+  }
+
+  findPaginatedCustomFields(pagination:Pagination){
+    this.customResponse = new CustomResponse();
+    this.referenceService.loading(this.httpRequestLoader, true);
+    this.customFieldService.findPaginatedCustomFields(pagination).subscribe(
+      response=>{
+        const data = response.data;
+        let isSuccess = response.statusCode === 200;
+        if(isSuccess){
+          pagination.totalRecords = data.totalRecords;
+          pagination = this.pagerService.getPagedItems(pagination, data.list);
+        }else{
+          this.customResponse = new CustomResponse('ERROR',"Unable to get custom fields.",true);
+        }
+        this.referenceService.loading(this.httpRequestLoader, false);
+      },error=>{
+        let message = this.referenceService.getApiErrorMessage(error);
+        this.customResponse = new CustomResponse('ERROR',message,true);
+        this.referenceService.loading(this.httpRequestLoader, false);
+      }
+    )
   }
 
 }
--- src/app/dashboard/user-profile/services/custom-field.service.ts
+++ src/app/dashboard/user-profile/services/custom-field.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Pagination } from 'app/core/models/pagination';
+import { AuthenticationService } from 'app/core/services/authentication.service';
+import { ReferenceService } from 'app/core/services/reference.service';
 
 @Injectable()
 export class CustomFieldService {
 
-  constructor() { }
+  URL = this.authenticationService.REST_URL;
+  ACCESS_TOKEN_SUFFIX_URL = "?access_token=";
+  CUSTOM_FIELD_PREFIX_URL = this.authenticationService.REST_URL + "custom-fields";
+  CUSTOM_FIELD_URL = this.CUSTOM_FIELD_PREFIX_URL+this.ACCESS_TOKEN_SUFFIX_URL;
+
+  constructor(public authenticationService:AuthenticationService,public referenceService:ReferenceService) { }
+
+  findPaginatedCustomFields(pagination:Pagination){
+    let userId = this.authenticationService.getUserId();
+    let pageableUrl = this.referenceService.getPagebleUrl(pagination);
+    let findAllUrl = this.CUSTOM_FIELD_PREFIX_URL+'/paginated'+'/userId/'+userId+this.ACCESS_TOKEN_SUFFIX_URL+this.authenticationService.access_token+pageableUrl;
+    return this.authenticationService.callGetMethod(findAllUrl);
+  }
 
 }
