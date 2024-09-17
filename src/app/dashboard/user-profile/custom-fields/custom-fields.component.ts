a5d7affe9367dc860e9702cec015fb3c7c9e59e1
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-09-17 15:04:20 +0530

    custom field component Added XNFR-679

--- src/app/dashboard/user-profile/custom-fields/custom-fields.component.ts
+++ src/app/dashboard/user-profile/custom-fields/custom-fields.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Properties } from 'app/common/models/properties';
+import { HttpRequestLoader } from 'app/core/models/http-request-loader';
+import { SortOption } from 'app/core/models/sort-option';
+import { AuthenticationService } from 'app/core/services/authentication.service';
+import { ReferenceService } from 'app/core/services/reference.service';
+
 
 @Component({
   selector: 'app-custom-fields',
   templateUrl: './custom-fields.component.html',
-  styleUrls: ['./custom-fields.component.css']
+  styleUrls: ['./custom-fields.component.css'],
+  providers:[SortOption,HttpRequestLoader]
 })
 export class CustomFieldsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authenticationService:AuthenticationService,public referenceService:ReferenceService,public sortOption:SortOption) { }
 
   ngOnInit() {
   }
