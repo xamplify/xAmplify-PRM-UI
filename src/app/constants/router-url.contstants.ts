73cbd8727733d680710a0231238ad58cd93a05bc
bsudheer <bsudheer@bsudheer-sa.stratapps.com>
2024-09-19 18:53:17 +0530

    Added router url for addd lead and add deal

--- src/app/constants/router-url.contstants.ts
+++ src/app/constants/router-url.contstants.ts
@@ -1,12 +1,18 @@
 export const RouterUrlConstants = {
-    home : '/home/',
-    dam  : 'dam/',
-    dashboard  : 'dashboard/',
+    home: '/home/',
+    dam: 'dam/',
+    dashboard: 'dashboard/',
     damPartnerCompanyAnalytics: 'partner-companies/',
-    damPartnerAnalytics:'partner-analytics/',
+    damPartnerAnalytics: 'partner-analytics/',
     quickLinks: 'quick-links',
-    superAdmin:'superAdmin/',
-    myProfile:'myProfile/',
-    chatGptSettings:'chatGPTSettings/'
-   
+    superAdmin: 'superAdmin/',
+    myProfile: 'myProfile/',
+    chatGptSettings: 'chatGPTSettings/',
+    addLead: 'leads/add',
+    manageLeads: 'leads/manage',
+    addDeal: 'deal/add',
+    manageDeals: 'deal/manage',
+    addLeadFromHome: 'leads/d/add',
+    addDealFromHome: 'deal/d/add'
+    
 }
