135bdf6f19f15b829c2096fe13b56cf33274fae6
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-10-01 21:59:04 +0530

    URL Updated

--- src/app/core/services/url-auth-guard.service.ts
+++ src/app/core/services/url-auth-guard.service.ts
@@ -11,7 +11,7 @@ export class UrlAuthGuardService {
   private DAM_URL = this.authenticationService.REST_URL + RouterUrlConstants.dam;
   private ACCESS_TOKEN_PARAMETER = XAMPLIFY_CONSTANTS.ACCESS_TOKEN_SUFFIX_URL;
   private userId = this.authenticationService.getUserId();
-  private checkDamModuleAccessURL = "/authorizeDamUrlAccess/loggedInUserId/";
+  private checkDamModuleAccessURL = "authorizeDamUrlAccess/";
 
   constructor(private authenticationService:AuthenticationService) {
 
@@ -21,9 +21,9 @@ export class UrlAuthGuardService {
     let domainName = this.authenticationService.getSubDomain();
     let url = "";
     if(domainName.length>0){
-      url = this.DAM_URL+"subDomain/"+domainName+this.checkDamModuleAccessURL+this.userId+this.ACCESS_TOKEN_PARAMETER+this.authenticationService.access_token;;
+      url = this.DAM_URL+this.checkDamModuleAccessURL+"subDomain/"+domainName+"/loggedInUserId/"+this.userId+this.ACCESS_TOKEN_PARAMETER+this.authenticationService.access_token;;
     }else{
-      url = this.DAM_URL+this.checkDamModuleAccessURL+this.userId+this.ACCESS_TOKEN_PARAMETER+this.authenticationService.access_token;;
+      url = this.DAM_URL+this.checkDamModuleAccessURL+"loggedInUserId/"+this.userId+this.ACCESS_TOKEN_PARAMETER+this.authenticationService.access_token;;
     }
     return this.authenticationService.callGetMethod(url);
 
