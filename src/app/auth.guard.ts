9b72e0b9cfcad68d548a2103ca80d0d5f73013cc
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2024-10-07 08:04:54 +0530

    Below Jira issues have been resolved.
    https://xamplify.atlassian.net/browse/XBI-3238
    https://xamplify.atlassian.net/browse/XBI-3235
    https://xamplify.atlassian.net/browse/XBI-3233
    https://xamplify.atlassian.net/browse/XBI-3225
    https://xamplify.atlassian.net/browse/XBI-3223
    https://xamplify.atlassian.net/browse/XBI-3164
    https://xamplify.atlassian.net/browse/XBI-3132
    https://xamplify.atlassian.net/browse/XBI-3127
    https://xamplify.atlassian.net/browse/XBI-3126
    https://xamplify.atlassian.net/browse/XBI-3078
    https://xamplify.atlassian.net/browse/XBI-3077
    https://xamplify.atlassian.net/browse/XBI-3071
    https://xamplify.atlassian.net/browse/XBI-3067
    https://xamplify.atlassian.net/browse/XBI-2896

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -408,7 +408,7 @@ export class AuthGuard implements CanActivate, CanActivateChild {
       }
 
     goToAccessDenied(url):boolean{
-        if(!(url.includes('/home/team/add-team') && this.utilService.isLoggedAsTeamMember())){
+        if(!(url.includes('/home/team/add-team') && this.utilService.isLoggedAsTeamMember()) && !url.includes("/home/partners/analytics")){
             this.router.navigate( ['/access-denied'] );
             return false;
         }else{
