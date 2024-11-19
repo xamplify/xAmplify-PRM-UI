79b5b6ed198209f6e5b8bb564ee74db7415ee9e6
gsaikumar <gsaikumar@gsaikumar-1-sa.stratapps.com>
2024-11-19 17:22:49 +0530

    Added preview email activity modal popup.

--- src/app/activity/preview-email-activity/preview-email-activity.component.ts
+++ src/app/activity/preview-email-activity/preview-email-activity.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { ReferenceService } from 'app/core/services/reference.service';
+import { EmailActivityService } from '../services/email-activity-service';
+import { EmailActivity } from '../models/email-activity-dto';
 
 @Component({
   selector: 'app-preview-email-activity',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewEmailActivityComponent implements OnInit {
 
-  constructor() { }
+  @Input() emailActivityId:any;
+  @Output() notifyClose = new EventEmitter();
+
+  ngxLoading:boolean = false;
+  emailActivity:EmailActivity = new EmailActivity();
+
+  constructor(public referenceService: ReferenceService, public emailActivityService:EmailActivityService) { }
 
   ngOnInit() {
+    this.fetchEmailActivityById();
+    this.referenceService.openModalPopup('previewEmailModalPopup');
+  }
+
+  fetchEmailActivityById() {
+    this.ngxLoading = true;
+    this.emailActivityService.fetchEmailActivityById(this.emailActivityId).subscribe(
+      data => {
+        if (data.statusCode == 200) {
+          this.emailActivity = data.data;
+        }
+        this.ngxLoading = false;
+      }, error => {
+        this.ngxLoading = false;
+      }
+    )
+  }
+
+  closeEmailModal() {
+    this.referenceService.closeModalPopup('previewEmailModalPopup');
+    this.notifyClose.emit();
   }
 
 }
