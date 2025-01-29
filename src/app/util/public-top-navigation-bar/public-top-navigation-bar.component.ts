dd95695e6bfcd14d9d0c0fd2dcba9ef3226289db
Chowdam-Sravan-Kumar <csravan@stratapps.com>
2025-01-29 18:50:14 +0530

    Modal Popup Added For Request Account

--- src/app/util/public-top-navigation-bar/public-top-navigation-bar.component.ts
+++ src/app/util/public-top-navigation-bar/public-top-navigation-bar.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { RequestDemo } from 'app/authentication/request-demo/request-demo';
+import { Country } from 'app/core/models/country';
+import { Timezone } from 'app/core/models/timezone';
 import { AuthenticationService } from 'app/core/services/authentication.service';
 import { ReferenceService } from 'app/core/services/reference.service';
 import { VanityURLService } from 'app/vanity-url/services/vanity.url.service';
+declare var $:any;
 @Component({
   selector: 'app-public-top-navigation-bar',
   templateUrl: './public-top-navigation-bar.component.html',
@@ -13,28 +17,102 @@ export class PublicTopNavigationBarComponent implements OnInit {
   requestAccountButtonClicked = false;
   requestAccountButtonText = "Request An Account";
   alias = "";
-  @Input() companyLogoPath:string;
-  @Input() emailAddress:string;
-  constructor(public vanityUrlService:VanityURLService,public authenticationService:AuthenticationService,public referenceService:ReferenceService) { }
+  @Input() companyLogoPath: string;
+  @Input() emailAddress: string;
+  processing = false;
+  isValidForm = false;
+
+  countries: Country[];
+  timezones: Timezone[];
+  requestDemo: RequestDemo = new RequestDemo();
+  isValidEmail = false;
+  isValidCompany = false;
+  isValidMobileNumber = false;
+  errorClass = "success";
+  submitted = false;
+  isContacted = false;
+  constructor(public vanityUrlService: VanityURLService, public authenticationService: AuthenticationService, public referenceService: ReferenceService) {
+    this.countries = this.referenceService.getCountries();
+    this.setCountry();
+  }
 
   ngOnInit() {
   }
 
-  requestAccount(){
+  setCountry() {
+    this.requestDemo.countryId = this.countries[0].id;
+    this.onSelect(this.requestDemo.countryId);
+  }
+
+  onSelect(countryId) {
+    this.timezones = this.referenceService.getTimeZonesByCountryId(countryId);
+  }
+
+
+  validateEmailId() {
+    if (!this.referenceService.validateEmailId($.trim(this.requestDemo.emailId))) {
+      this.isValidEmail = false;
+      this.isValidForm = false;
+      this.errorClass = "error";
+    } else {
+      this.isValidEmail = true;
+      this.errorClass = "success";
+      if (this.isValidCompany && this.validateMobileNumber) {
+        this.isValidForm = true;
+      } else {
+        this.isValidForm = false;
+      }
+    }
+  }
+
+  validateCompany() {
+    let company = $.trim(this.requestDemo.company);
+    if (company.length > 0) {
+      this.isValidCompany = true;
+      if (this.isValidEmail && this.validateMobileNumber) {
+        this.isValidForm = true;
+      } else {
+        this.isValidForm = false;
+      }
+    } else {
+      this.isValidCompany = false;
+      this.isValidForm = false;
+    }
+  }
+
+  validateMobileNumber() {
+    let mobileNumber = $.trim(this.requestDemo.mobileNumber);
+    if (mobileNumber.length > 0) {
+      this.isValidMobileNumber = true;
+      if (this.isValidEmail && this.isValidCompany) {
+        this.isValidForm = true;
+      } else {
+        this.isValidForm = false;
+      }
+    } else {
+      this.isValidMobileNumber = false;
+      this.isValidForm = false;
+    }
+  }
+
+
+  requestAccount() {
     this.requestAccountButtonClicked = true;
     this.requestAccountButtonText = "Please Wait...";
-    let campaignMdfRequestAccountDto= {};
+    let campaignMdfRequestAccountDto = {};
     campaignMdfRequestAccountDto['mdfAlias'] = this.alias;
     this.authenticationService.requestAccount(campaignMdfRequestAccountDto).subscribe(
       response => {
         this.referenceService.showSweetAlertSuccessMessage("Your account request has been submitted successfully");
         this.requestAccountButtonClicked = false;
-        this.requestAccountButtonText = "Request an Account";
       }, error => {
         this.requestAccountButtonClicked = false;
-        this.requestAccountButtonText = "Request an Account";
-       this.referenceService.showSweetAlertServerErrorMessage();
+        this.referenceService.showSweetAlertServerErrorMessage();
       });
+  }
+
+  openRequestAccountModal() {
+    this.referenceService.openModalPopup("request-account-modal-popup");
   }
 
 
