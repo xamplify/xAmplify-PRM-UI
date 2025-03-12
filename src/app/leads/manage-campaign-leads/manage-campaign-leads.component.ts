f32999a946c13fb0ee8a899c86d8415a0fe85875
gsaikumar <gsaikumar@stratapps.com>
2025-03-12 23:06:07 +0530

    XBI-4155 is fixed.

--- src/app/leads/manage-campaign-leads/manage-campaign-leads.component.ts
+++ src/app/leads/manage-campaign-leads/manage-campaign-leads.component.ts
@@ -550,7 +550,7 @@ showRegisterDealButton(lead):boolean {
   if (lead.selfLead && lead.dealBySelfLead && (this.authenticationService.module.isOrgAdminCompany || this.authenticationService.module.isMarketingCompany) && lead.associatedDealId == undefined) {
     showRegisterDeal = true;
   } else if (((((lead.dealByVendor && (this.authenticationService.module.isVendor || this.authenticationService.isVendor() || this.authenticationService.isOrgAdmin()) || lead.canRegisterDeal && lead.dealByPartner) && !lead.selfLead)) && lead.associatedDealId == undefined) 
-    && ((lead.enableRegisterDealButton && !lead.leadApprovalOrRejection && !this.authenticationService.module.deletedPartner && lead.leadApprovalStatusType !== 'REJECTED'))) {
+    && ((lead.enableRegisterDealButton && !lead.leadApprovalOrRejection && !this.authenticationService.module.deletedPartner && lead.leadApprovalStatusType !== 'REJECTED')) && (this.authenticationService.enableLeads || this.authenticationService.module.opportunitiesAccessAsPartner)) {
     showRegisterDeal = true;
   }
   return showRegisterDeal;
