96803291f5d15fcc613fc420fb9b37937201c702
pvamsilakshman999 <pvamsi@stratapps.com>
2025-03-23 20:28:08 +0530

    write and chat with oliver

--- src/app/common/chat-gpt-modal/chat-gpt-modal.component.ts
+++ src/app/common/chat-gpt-modal/chat-gpt-modal.component.ts
@@ -5,6 +5,7 @@ import { ChatGptSettingsService } from 'app/dashboard/chat-gpt-settings.service'
 import { CustomResponse } from '../models/custom-response';
 import { Properties } from '../models/properties';
 import { SortOption } from 'app/core/models/sort-option';
+import { ChatGptIntegrationSettingsDto } from 'app/dashboard/models/chat-gpt-integration-settings-dto';
 declare var $:any;
 @Component({
   selector: 'app-chat-gpt-modal',
@@ -24,27 +25,43 @@ export class ChatGptModalComponent implements OnInit {
   customResponse:CustomResponse = new CustomResponse();
   showIcon: boolean = true;
   activeTab: string = 'paraphraser';
+  selectedValueForWork : any ={};
+  chatGptIntegrationSettingsDto = new ChatGptIntegrationSettingsDto();
+  actionType: string ;
+  showEmailModalPopup: boolean;
+  emailBody: any;
+  subjectText: string;
   constructor(public authenticationService:AuthenticationService,private chatGptSettingsService:ChatGptSettingsService,
     private referenceService:ReferenceService,public properties:Properties,public sortOption:SortOption) { 
     
   }
 
   ngOnInit() {
+    this.selectedValueForWork = this.sortOption.wordOptionsForOliver[0].value;
+    this.sortBy(this.selectedValueForWork);
+
   }
+  
 
 
   validateInputText(){
     let trimmedText = this.referenceService.getTrimmedData(this.inputText);
     this.isValidInputText = trimmedText!=undefined && trimmedText.length>0;
   }
-
+  ngOnDestroy() {
+    this.showIcon = true;
+  }
  
   generateChatGPTText(){
     this.customResponse = new CustomResponse();
     this.isTextLoading = true;
     this.chatGptGeneratedText = '';
     // let askOliver = 'Paraphrase this:' + this.inputText
-    this.chatGptSettingsService.generateText(this.inputText).subscribe(
+    let askOliver = this.activeTab == 'writing'
+    ? 'In ' + (this.sortOption.selectWordDropDownForOliver.name || '') + ' ' + this.inputText
+    : this.inputText;
+    this.chatGptIntegrationSettingsDto.prompt = askOliver;
+    this.chatGptSettingsService.generateAssistantText(this.chatGptIntegrationSettingsDto).subscribe(
       response=>{
         let statusCode = response.statusCode;
         let data = response.data;
@@ -88,10 +105,56 @@ export class ChatGptModalComponent implements OnInit {
     this.showIcon = true;
   }
 
-  sortBy(text: any) {
-    this.sortOption.selectWordDropDownForOliver = text;
+  sortBy(selectedValue: string) {
+    this.selectedValueForWork = selectedValue;
+    this.sortOption.selectWordDropDownForOliver = this.sortOption.wordOptionsForOliver.find(
+      option => option.value === selectedValue
+    );
   }
+  
   setActiveTab(tab: string) {
+    this.isValidInputText = false;
+    this.inputText ="";
+    this.isTextLoading = false;
     this.activeTab = tab;
+  }
+  openEmailModalPopup(markdown: any) {
+    let text = markdown.innerHTML;
+    if (text != undefined) {
+      this.emailBody = text.replace(/<\/?markdown[^>]*>/g, '');
+    }
+    this.parseHTMLBody(this.emailBody);
+    this.actionType = 'oliveAi';
+    this.showEmailModalPopup = true;
+  }
+  parseHTMLBody(emailContent: string): void {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = emailContent;
+    let plainText = tempDiv.innerHTML;
+    let subject = "";
+    let body = "";
+    let subjectStartIndex = plainText.indexOf("<p>Subject:");
+    if (subjectStartIndex !== -1) {
+      let subjectEndIndex = plainText.indexOf("</p>", subjectStartIndex);
+      if (subjectEndIndex !== -1) {
+        this.subjectText = plainText.substring(subjectStartIndex + 3, subjectEndIndex)
+          .replace("Subject:", "").trim();
+      }
+      this.emailBody = plainText.substring(subjectEndIndex + 4).trim();
+      let lastHrIndex = this.emailBody.lastIndexOf("<hr");
+      if (lastHrIndex !== -1) {
+        this.emailBody = this.emailBody.substring(0, lastHrIndex).trim();
+      }
+    }
+    console.log("Subject:", subject);
+    console.log("Body:", body);
+  }
+  closeEmailModalPopup(event: any) {
+    this.showEmailModalPopup = false;
+    this.subjectText = "";
+    this.emailBody = "";
+    if (event) {
+    this.referenceService.showSweetAlertSuccessMessage(event);
+    }
   }
 }
