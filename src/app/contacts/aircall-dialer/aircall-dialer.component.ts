bb3ecc15f4e159d8104036044d8bb0d695877ae0
gsaikumar <gsaikumar@stratapps.com>
2025-03-25 09:45:16 +0530

    Removed commented code.

--- src/app/contacts/aircall-dialer/aircall-dialer.component.ts
+++ src/app/contacts/aircall-dialer/aircall-dialer.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { ReferenceService } from 'app/core/services/reference.service';
 
 @Component({
@@ -12,53 +12,15 @@ export class AircallDialerComponent implements OnInit {
 
   @Output() notifyClose = new EventEmitter();
 
-  @ViewChild('aircallIframe') iframeRef: ElementRef;
-
   constructor(private referenceService: ReferenceService) { }
 
   ngOnInit() {
     this.referenceService.openModalPopup('addCallModalPopup');
-    this.sendCSSMessage();
   }
 
   closeCallModal() {
     this.referenceService.closeModalPopup('addCallModalPopup');
     this.notifyClose.emit(!this.isReloadTab);
-  }
-
-  ngAfterViewInit() {
-    // this.iframeRef.nativeElement.addEventListener('load', () => {
-    //   const iframeDocument = this.iframeRef.nativeElement.contentDocument ||
-    //     this.iframeRef.nativeElement.contentWindow.document;
-
-    //   if (iframeDocument) {
-    //     const style = iframeDocument.createElement('style');
-    //     style.innerHTML = `
-    //       .hKkIYA {
-    //           flex: 1 1 auto;
-    //           flex-direction: column;
-    //           overflow: scroll;
-    //           width: 100%;
-    //       }
-    //     `;
-    //     iframeDocument.head.appendChild(style);
-    //   }
-    // });
-  }
-
-  sendCSSMessage() {
-    const cssMessage = {
-      type: 'inject-css',
-      styles: `
-        .hKkIYA {
-            flex: 1 1 auto;
-            flex-direction: column;
-            overflow: scroll;
-            width: 100%;
-        }
-      `
-    };
-    this.iframeRef.nativeElement.contentWindow.postMessage(cssMessage, '*');
   }
 
 }
