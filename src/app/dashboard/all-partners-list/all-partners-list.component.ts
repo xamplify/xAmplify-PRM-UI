6f3eeb397820c75afa920f432ca629b02ac3c559
shailajap0308 <pshailaja@stratapps.com>
2025-05-02 10:38:25 +0530

    1. Region is selected from Donut slice and more regions added in the filter section,later after when we search something in search box, search text need to be find in all the regions of filter section.
    2. Removed commented code.

--- src/app/dashboard/all-partners-list/all-partners-list.component.ts
+++ src/app/dashboard/all-partners-list/all-partners-list.component.ts
@@ -77,7 +77,6 @@ export class AllPartnersListComponent implements OnInit {
 
   ngOnChanges() {
     this.pagination.pageIndex = 1;
-    //this.setHeading();
     this.getAllPartnersDetails(this.pagination);
     this.findRegionNames();
     this.setFilterColor();
@@ -109,17 +108,11 @@ export class AllPartnersListComponent implements OnInit {
   clearFilter() {
     this.pagination.fromDateFilterString = '';
     this.pagination.toDateFilterString = '';
-     this.pagination.selectedStatusIds = [];
-     this.pagination.selectedRegionIds = [];
-     this.regionName =''
-     this.pagination.regionFilter = this.regionName;
-    // this.selectedStatusIds = [];
-    // this.selectedRegionIds = [];
-   // this.regionNameFilters = [];
+    this.pagination.selectedStatusIds = [];
+    this.pagination.selectedRegionIds = [];
+    this.regionName =''
+    this.pagination.regionFilter = this.regionName;
     this.isCollapsed = true;
-    //this.pagination.filterBy = [];
-    //this.filterApplied = false;
-   // this.showFilterOption = true;
     this.filterActiveBg = 'defaultFilterACtiveBg';
     this.getAllPartnersDetails(this.pagination);
 }
@@ -130,7 +123,6 @@ setFilterColor() {
   let isValidToDateFilter = this.pagination.toDateFilterString != undefined && this.pagination.toDateFilterString.length > 0;
   if (isValidSelectedRegionAndStatus && isValidFromDateFilter && isValidToDateFilter) {
     this.filterActiveBg = 'filterActiveBg';
-   // this.filterApplied = true;
   }
   if (isValidSelectedRegionAndStatus) {
     this.filterActiveBg = 'filterActiveBg';
@@ -174,11 +166,9 @@ validateDateFilter() {
   } else {
     this.applyFilters(this.pagination);
   }
- // this.applyFilters(this.pagination);
 }
 applyFilters(pagination: Pagination) {
   this.filterActiveBg = 'filterActiveBg';
- // this.filterApplied = true;
   this.clickFilter();
   this.referenseService.loading(this.httpRequestLoader, true);
   pagination.pageIndex = 1;
@@ -192,63 +182,7 @@ applyFilters(pagination: Pagination) {
     this.pagination.regionFilter = '';
   }
   this.getAllPartnersDetailsList(this.pagination);
- // this.pagination.selectedRegionIds = this.selectedRegionIds;
- // this.pagination.selectedStatusIds = this.selectedStatusIds;
- /* this.parterService.getAllPartners(pagination).subscribe(
-    (response: any) => {
-      this.referenseService.loading(this.httpRequestLoader, false);
-      if (response.statusCode == 200) {
-      this.sortOption.totalRecords = response.data.totalRecords;
-        this.pagination.totalRecords = response.data.totalRecords;
-        if (this.pagination.totalRecords == 0) {
-          this.scrollClass = 'noData'
-        } else {
-          this.scrollClass = 'tableHeightScroll'
-        }
-        this.pagination = this.pagerService.getPagedItems(this.pagination, response.data.list);
-      }
-    },
-    (_error: any) => {
-      this.httpRequestLoader.isServerError = true;
-      this.xtremandLogger.error(_error);
-    }
-  );*/
-
-}
-  
-
-  // getInteractedNotInteractedTrackDetailsForPartnerJourney(pagination: Pagination) {
-  //   this.referenseService.loading(this.httpRequestLoader, true);
-  //   this.pagination.userId = this.loggedInUserId;
-  //   this.pagination.partnerCompanyId = this.partnerCompanyId;
-  //   this.pagination.selectedPartnerCompanyIds = this.selectedPartnerCompanyIds;
-  //   this.pagination.detailedAnalytics = this.isDetailedAnalytics;
-  //   this.pagination.trackTypeFilter = this.trackType;
-  //   this.pagination.partnerTeamMemberGroupFilter = this.applyFilter;
-  //   this.pagination.teamMemberId = this.teamMemberId;
-  //   this.pagination.fromDateFilterString = this.fromDateFilter;
-  //   this.pagination.toDateFilterString = this.toDateFilter;
-  //   this.pagination.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  //   this.parterService.getPartnerJourneyTrackDetailsByInteraction(this.pagination).subscribe(
-  //     (response: any) => {
-  //       this.referenseService.loading(this.httpRequestLoader, false);
-  //       if (response.statusCode == 200) {
-  //         this.sortOption.totalRecords = response.data.totalRecords;
-  //         this.pagination.totalRecords = response.data.totalRecords;
-  //         if (pagination.totalRecords == 0) {
-  //           this.scrollClass = 'noData'
-  //         } else {
-  //           this.scrollClass = 'tableHeightScroll'
-  //         }
-  //         this.pagination = this.pagerService.getPagedItems(this.pagination, response.data.list);
-  //       }
-  //     },
-  //     (_error: any) => {
-  //       this.httpRequestLoader.isServerError = true;
-  //       this.xtremandLogger.error(_error);
-  //     }
-  //   );
-  // }
+ }
 
   search() {
     this.getAllFilteredResults(this.pagination);
@@ -261,13 +195,23 @@ applyFilters(pagination: Pagination) {
   }
 
   getAllFilteredResults(pagination: Pagination) {
+    this.referenseService.loading(this.httpRequestLoader, true);
+    this.pagination.userId = this.loggedInUserId;
+    let regionFilter = this.regionName;
     pagination.pageIndex = 1;
     pagination.searchKey = this.searchKey;
+    pagination.selectedRegionIds=this.pagination.selectedRegionIds;
+    pagination.selectedStatusIds=this.pagination.selectedStatusIds;
     pagination = this.utilService.sortOptionValues(this.sortOption.teamMember, pagination);
-   // let sortColumn = this.pagination.sortcolumn;
-   // pagination.sortcolumn = sortColumn;
-    this.getAllPartnersDetails(this.pagination);
-   // this.getInteractedNotInteractedTrackDetails(this.pagination);
+    if (this.pagination.selectedRegionIds && this.pagination.selectedRegionIds.includes(this.regionName)) {
+      regionFilter = '';
+    }
+    this.pagination = pagination;
+    //this.getAllPartnersDetails(this.pagination);
+    this.pagination.regionFilter = regionFilter;
+    this.pagination.toDateFilterString = this.pagination.toDateFilterString;
+    this.pagination.fromDateFilterString = this.pagination.fromDateFilterString;
+    this.getAllPartnersDetailsList(this.pagination);
   }
 
    setPage(event: any) {
@@ -338,106 +282,4 @@ applyFilters(pagination: Pagination) {
     window.location.href = this.authenticationService.REST_URL + '/partner/allPartners/downloadCsv?userId='
       + loggedInUserId +"&regionFilter="+regionFilter+"&sortColumn="+sortColumn+"&selectedRegionIds="+selectedRegionIds+"&selectedStatusIds="+selectedStatusIds+"&access_token=" + this.authenticationService.access_token + pageableUrl;
    }
-  // getInteractedNotInteractedTrackDetails(pagination: Pagination) {
-  //   if (!this.isTeamMemberAnalytics) {
-  //     this.getInteractedNotInteractedTrackDetailsForPartnerJourney(this.pagination);
-  //   } else {
-  //     this.getAllPartnersDetails(this.pagination);
-  //   }
-  // }
-
-  // downloadInteractedAndNonInteractedTracksReport() {
-  //   if (!this.isTeamMemberAnalytics) {
-  //     this.downloadInteractedAndNonInteractedTracksReportForPartnerJourney(this.pagination);
-  //   } else {
-  //     this.downloadInteractedAndNonInteractedTracksReportForTeamMember(this.pagination);
-  //   }
-  // }
-
-  // downloadInteractedAndNonInteractedTracksReportForPartnerJourney(pagination: Pagination) {
-  //   let loggedInUserIdRequestParam = this.loggedInUserId != undefined && this.loggedInUserId > 0 ? this.loggedInUserId : 0;
-  //   let trackTypeFilterRequestParam = this.trackType != undefined ? this.trackType : "";
-  //   let partnerCompanyIdsRequestParam = this.selectedPartnerCompanyIds && this.selectedPartnerCompanyIds.length > 0 ? this.selectedPartnerCompanyIds : [];
-  //   let searchKeyRequestParm = this.searchKey != undefined ? this.sortOption.searchKey : "";
-  //   let partnerCompanyIdRequestParam = this.partnerCompanyId != undefined && this.partnerCompanyId > 0 ? this.partnerCompanyId : 0;
-  //   let partnerTeamMemberGroupFilterRequestParm = this.applyFilter != undefined ? this.applyFilter : false;
-  //   let teamMemberIdRequestParam = this.teamMemberId != undefined && this.teamMemberId > 0 ? this.teamMemberId : 0;
-  //   let fromDateFilterRequestParam = this.fromDateFilter != undefined ? this.fromDateFilter : "";
-  //   let toDateFilterRequestParam = this.toDateFilter != undefined ? this.toDateFilter : "";
-  //   let timeZoneRequestParm = "&timeZone=" + Intl.DateTimeFormat().resolvedOptions().timeZone;
-  //   let url = this.authenticationService.REST_URL + "partner/journey/download/track-interaction-report?access_token=" + this.authenticationService.access_token
-  //     + "&loggedInUserId=" + loggedInUserIdRequestParam + "&trackTypeFilter=" + trackTypeFilterRequestParam
-  //     + "&selectedPartnerCompanyIds=" + partnerCompanyIdsRequestParam + "&searchKey=" + searchKeyRequestParm + "&detailedAnalytics=" + this.isDetailedAnalytics + "&partnerCompanyId=" + partnerCompanyIdRequestParam
-  //     + "&partnerTeamMemberGroupFilter=" + partnerTeamMemberGroupFilterRequestParm + "&teamMemberUserId=" + teamMemberIdRequestParam
-  //     + "&fromDateFilterInString=" + fromDateFilterRequestParam + "&toDateFilterInString=" + toDateFilterRequestParam + timeZoneRequestParm;
-  //   this.referenseService.openWindowInNewTab(url);
-  // }
-
-  // downloadInteractedAndNonInteractedTracksReportForTeamMember(pagination: Pagination) {
-  //   this.setPaginationValuesForTeamMember();
-  //   let teamMemberAnalyticsUrl = this.referenseService.getTeamMemberAnalyticsUrl(this.pagination);
-  //   let isVendorVersionRequestParam = this.isVendorVersion ? "&vendorVersion=" + this.isVendorVersion : "";
-  //   let url = this.authenticationService.REST_URL + "teamMemberAnalytics/download/track-interaction-report?access_token=" + this.authenticationService.access_token + teamMemberAnalyticsUrl
-  //     + isVendorVersionRequestParam;
-  //   this.referenseService.openWindowInNewTab(url);
-  // }
-
-  //setPaginationValuesForTeamMember() {
-   // this.pagination.userId = this.loggedInUserId;
-  // this.pagination.regionFilter = this.regionName;
-    //this.pagination.selectedTeamMemberIds = this.selectedTeamMemberIds;
-   // this.pagination.selectedVendorCompanyIds = this.selectedVendorCompanyIds;
-    // if (!this.isVendorVersion) {
-    //   this.pagination.vanityUrlFilter = this.vanityUrlFilter;
-    //   this.pagination.vendorCompanyProfileName = this.vendorCompanyProfileName;
-    // }
-    // this.pagination.fromDateFilterString = this.fromDateFilter;
-    // this.pagination.toDateFilterString = this.toDateFilter;
-    // this.pagination.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  //}
- resendEmailInvitation(emailId: string,status: string) {
-      let self = this;
-      swal({
-        title: 'Are you sure?',
-        text: "An invitation email will be sent to team member",
-        type: 'warning',
-        showCancelButton: true,
-        swalConfirmButtonColor: '#54a7e9',
-        swalCancelButtonColor: '#999',
-        confirmButtonText: 'Yes, send it!'
-
-      }).then(function () {
-        self.sendEmail(emailId,status);
-      }, function (dismiss: any) {
-        console.log('you clicked on option' + dismiss);
-      });
-  }
-
-  sendEmail(emailId: string,status:string) {
-    this.customResponse = new CustomResponse();
-    try {
-      this.loading = true;
-      let input = {};
-      input['userId'] = this.authenticationService.getUserId();
-      input['emailId'] = emailId;
-      input['status'] = status;
-      // this.teamMemberService.resendTeamMemberInvitation(input)
-      //   .subscribe(
-      //     data => {
-      //       if (data.statusCode == 200) {
-      //         this.customResponse = new CustomResponse('SUCCESS', "Invitation sent successfully.", true);
-      //       } else {
-      //         this.customResponse = new CustomResponse('ERROR', "Invitation cannot be sent as the account is already created for team member", true);
-      //       }
-      //       this.loading = false;
-      //     },
-      //     error => {
-      //       this.loading = false;
-      //       this.logger.errorPage(error);
-      //     }
-      //   );
-    } catch (error) {
-      this.loading = false;
-    }
-  }
 }
