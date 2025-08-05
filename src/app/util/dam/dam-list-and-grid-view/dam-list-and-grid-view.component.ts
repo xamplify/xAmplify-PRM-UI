fca6d817e0fe90ae136be04e389781adafa77752
PVamsiLakshman <vamsi@xamplify.com>
2025-08-05 10:30:02 +0530

    https://xamplify.atlassian.net/browse/XBI-5136 is resolved and icon changed in insights

--- src/app/util/dam/dam-list-and-grid-view/dam-list-and-grid-view.component.ts
+++ src/app/util/dam/dam-list-and-grid-view/dam-list-and-grid-view.component.ts
@@ -720,8 +720,13 @@ export class DamListAndGridViewComponent implements OnInit, OnDestroy {
 	/*****Preview Asset******* */
 	preview(asset: any) {
 		if (this.referenceService.isVideo(asset.assetType)) {
-			let url = "/home/dam/previewVideo/" + asset.videoId + "/" + asset.id;
-			this.referenceService.navigateToRouterByViewTypes(url, this.categoryId, this.viewType, this.folderViewType, this.folderListView);
+			if (this.FromOliverPopUp) {
+				let url = "/home/dam/previewVideo/" + asset.videoId + "/" + asset.id;
+				window.open(url, "_blank");
+			} else {
+				let url = "/home/dam/previewVideo/" + asset.videoId + "/" + asset.id;
+				this.referenceService.navigateToRouterByViewTypes(url, this.categoryId, this.viewType, this.folderViewType, this.folderListView);
+			}
 		} 
 		else if (asset.beeTemplate && (asset.assetPath == null || asset.assetPath == '' || asset.assetPath.length == 0)) {
 			this.referenceService.previewAssetPdfInNewTab(asset.id);
